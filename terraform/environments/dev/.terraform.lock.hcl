ff93a265d0600754797d947d57fa527e448bf1ea
Ian Rose <ian.rose@innovation.ca.gov>
2023-10-18 11:07:20 -0700

    Add Snowflake ELT module

--- terraform/environments/dev/.terraform.lock.hcl
+++ terraform/environments/dev/.terraform.lock.hcl
@@ -25,3 +25,22 @@ provider "registry.terraform.io/hashicorp/aws" {
     "zh:f78ec7a771867d96dfee96bf74523341ba42feeb64ce2f108b5bf2e7ebef0fef",
   ]
 }
+
+provider "registry.terraform.io/snowflake-labs/snowflake" {
+  version     = "0.69.0"
+  constraints = "~> 0.61, 0.69.0"
+  hashes = [
+    "h1:0CSDES330nTqimmmNwQxxp24dDkRGYTrz46wjJDBG9M=",
+    "h1:S57eACfdn03/8yUFRcSA52BsBsuEIJ0KwP5jhdoKMuY=",
+    "h1:rB052GXnpNuUR9ZeGgiY8imkQqPWkI32PtiqWTORJXQ=",
+    "zh:05c38443ecce2a74568a182eb8a796db5d333846552d75c16b158f5034e824a3",
+    "zh:38b549a2e09b911709c85340222533cff9fde9c0de3e83cb906891822b340279",
+    "zh:3d4468d3be703c0545db94f6f1861ffd0f6e6e40cbdad6cf22506fcf368d33cd",
+    "zh:43ae1d8dd68545923ad460593b3f9c6d0a010b66c8ece7c62e4c2c716e3bb848",
+    "zh:4f4bb057e411138f876cc87b676269f23d62ffeafd4bbcf7d96521ee8abb2dff",
+    "zh:51c3f30ec1ede2c002d103e0dfeee187ef69be77c7a948355f6e5a5c505745f2",
+    "zh:74e7fd7d960a2e7e069941fb665e2873c4ced8cf84d5e1f8aded98ad9bb742c8",
+    "zh:97330c429ba7c17eec8ee325695bdc81093e1c70a275fd70ffeb00719ffc73ce",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.56.0"
     }
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.69"
+    }
   }
 
   backend "s3" {
@@ -21,6 +25,7 @@ locals {
   environment = "dev"
   project     = "pems"
   region      = "us-west-2"
+  locator     = "NGB13288"
 }
 
 provider "aws" {
@@ -35,13 +40,61 @@ provider "aws" {
   }
 }
 
+# This provider is intentionally low-permission. In Snowflake, object creators are
+# the default owners of the object. To control the owner, we create different provider
+# blocks with different roles, and require that all snowflake resources explicitly
+# flag the role they want for the creator.
+provider "snowflake" {
+  account = local.locator
+  role    = "PUBLIC"
+}
+
+# Snowflake provider for creating databases, warehouses, etc.
+provider "snowflake" {
+  alias   = "sysadmin"
+  account = local.locator
+  role    = "SYSADMIN"
+}
+
+# Snowflake provider for managing grants to roles.
+provider "snowflake" {
+  alias   = "securityadmin"
+  account = local.locator
+  role    = "SECURITYADMIN"
+}
+
+# Snowflake provider for managing user accounts and roles.
+provider "snowflake" {
+  alias   = "useradmin"
+  account = local.locator
+  role    = "USERADMIN"
+}
+
 ############################
-#      Infrastructure      #
+#    AWS Infrastructure    #
 ############################
 
 module "s3_lake" {
   source = "../../modules/s3-lake"
+  providers = {
+    aws = aws
+  }
 
   prefix = "${local.owner}-${local.project}-${local.environment}"
   region = local.region
+}
+
+############################
+# Snowflake Infrastructure #
+############################
+
+module "elt" {
+  source = "github.com/cagov/data-infrastructure.git//terraform/snowflake/modules/elt?ref=74a522f"
+  providers = {
+    snowflake.securityadmin = snowflake.securityadmin,
+    snowflake.sysadmin      = snowflake.sysadmin,
+    snowflake.useradmin     = snowflake.useradmin,
+  }
+
+  environment = upper(local.environment)
 }
