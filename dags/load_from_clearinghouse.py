9c248ad0d734ac2eeb62db0cfe6621fbc8586f20
Ian Rose <ian.rose@innovation.ca.gov>
2023-11-16 07:32:27 -0800

    Having this be a set with undefined order was annoying

--- dags/load_from_clearinghouse.py
+++ dags/load_from_clearinghouse.py
@@ -10,7 +10,7 @@ from airflow.decorators import dag, task
 
 CLHOUSE_PREFIX = "https://pems.dot.ca.gov/feeds/clhouse"
 S3_PREFIX = "s3://caltrans-pems-dev-us-west-2-raw"
-DISTRICTS = { "d03", "d04", "d05", "d06", "d07", "d08", "d10", "d11", "d12" }
+DISTRICTS = [ "d03", "d04", "d05", "d06", "d07", "d08", "d10", "d11", "d12" ]
 
 def copy_file(src, dst, s3) -> None:
     with fsspec.open(src, "rb") as f:
