7b80cd5c8071dba2b8ff100603fd852971da8675
Britt Allen <britt.allen@innovation.ca.gov>
2024-02-08 23:15:53 +0000

    also committing for now

--- transform/models/intermediate/int_pems__stations_per_county_counted.sql
+++ transform/models/intermediate/int_pems__stations_per_county_counted.sql
@@ -9,8 +9,8 @@ stations_per_county_counted as (
         county_name,
         COUNT(id) as station_count
     from staging_station_meta
-    -- normally this kind of filtering would be done in the BI layer
     where meta_date between '2023-01-01' and '2023-12-31'
+    -- normally this kind of filtering would be done in the BI layer
     group by county_name
 )
 
