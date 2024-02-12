2aa557d352f6a0a1da9182d386592d2b4dac4bfd
pingping.xiu@dot.ca.gov <pingping.xiu@dot.ca.gov>
2024-02-12 22:23:33 +0000

    test

--- transform/models/staging/stg_pems__station_meta.sql
+++ transform/models/staging/stg_pems__station_meta.sql
@@ -1,3 +1,23 @@
-select *
+select
+    DATE_FROM_PARTS(
+        CAST(SUBSTRING(filename, 18, 4) as NUMBER),    -- Extract Year
+        CAST(SUBSTRING(filename, 23, 2) as NUMBER),    -- Extract Month
+        CAST(SUBSTRING(filename, 48, 2) as NUMBER)     -- Extract Day
+    ) as extracted_date,
+    -- filename,
+    -- id,
+    fwy,
+    dir,
+    district,
+    county,
+    city,
+    state_pm,
+    abs_pm,
+    latitude,
+    longitude,
+    length,
+    type,
+    lanes,
+    name
 from raw_prd.clearinghouse.station_meta
-limit 10
+limit 10000
