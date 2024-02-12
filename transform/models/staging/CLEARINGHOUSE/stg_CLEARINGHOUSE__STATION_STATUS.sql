e48f4a450abef5bb7cc908d0b5613ddd63990c77
Azam.beg@dot.ca.gov <Azam.beg@dot.ca.gov>
2024-02-12 23:39:14 +0000

    abc

---
+++ transform/models/staging/CLEARINGHOUSE/stg_CLEARINGHOUSE__STATION_STATUS.sql
@@ -0,0 +1,19 @@
+with 
+
+source as (
+
+    select * from {{ source('CLEARINGHOUSE', 'STATION_STATUS') }}
+
+),
+
+renamed as (
+
+    select
+        filename,
+        content
+
+    from source
+
+)
+
+select * from renamed
