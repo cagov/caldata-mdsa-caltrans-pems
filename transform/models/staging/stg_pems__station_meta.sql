535813f97ae56e9c1a992738255541e2ddcf5729
zhenyu zhu <zhenyu.zhu@dot.ca.gov>
2024-02-13 05:34:17 +0000

    day2

--- transform/models/staging/stg_pems__station_meta.sql
+++ transform/models/staging/stg_pems__station_meta.sql
@@ -1,23 +1,4 @@
 select
-    -- FILENAME,
-    ID,
-    FWY as FREEWAY_NO,
-    DIR as DIRECTION,
-    DISTRICT,
-    COUNTY,
-    CITY,
-    STATE_PM as STATE_POSTMILE,
-    ABS_PM as ABSOLUTE_POSTMILE,
-    LATITUDE,
-    LONGITUDE,
-    LENGTH,
-    TYPE,
-    LANES,
-    NAME,
-    CAST(
-        SUBSTRING(FILENAME, POSITION('_meta_' in FILENAME) + len('_meta_'), len('2024')) || '-'
-        || SUBSTRING(FILENAME, POSITION('_meta_' in FILENAME) + len('_meta_2024_'), len('01')) || '-'
-        || SUBSTRING(FILENAME, POSITION('_meta_' in FILENAME) + len('_meta_2024_01_'), len('31')) as Date
-    ) as META_DATE
-FROM raw_prd.clearinghouse.station_meta
+*
+FROM {{source('CLEARINGHOUSE', 'STATION_META')}}
 -- limit 10
