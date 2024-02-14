7f9ca66e95e58329a9b3a4f99ae0ba6bbf25701f
zhenyu zhu <zhenyu.zhu@dot.ca.gov>
2024-02-14 22:55:11 +0000

    create ref and CTE for a new sql

--- transform/models/intermediate/int_pems__stations_per_county_counted.sql
+++ transform/models/intermediate/int_pems__stations_per_county_counted.sql
@@ -1,3 +1,28 @@
-select *
-from stg_pems__station_meta
-limit 10
+with
+
+County_data as (
+    select
+        County,
+        COUNT(Id) as Station_counts
+    from {{ ref("stg_pems__station_meta") }}
+    where CAST(SUBSTRING(Meta_date, 1, 4) as INT) = 2023
+    group by County
+)
+
+select
+    *
+from County_data
+order by Station_counts asc
+
+
+-- select county, count(ID) as station_counts
+-- from {{ ref("stg_pems__station_meta") }}
+-- where  CAST(SUBSTRING(META_DATE, 1, 4) AS INT) = 2023
+-- Group by COUNTY
+-- Order by station_counts Desc
+-- limit 10
+
+
+-- select *
+-- from {{ ref("stg_pems__station_meta") }}
+-- limit 10
