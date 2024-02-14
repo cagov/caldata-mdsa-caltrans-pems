d9972501b0db8b16a1ff042e0d634cddfb27cab5
pingping.xiu@dot.ca.gov <pingping.xiu@dot.ca.gov>
2024-02-14 22:45:03 +0000

    test

--- transform/models/intermediate/int_pems__stations_per_county_counted.sql
+++ transform/models/intermediate/int_pems__stations_per_county_counted.sql
@@ -1,3 +1,36 @@
+
+with 
+source as (
+  --  select * from {{ source('CLEARINGHOUSE', 'STATION_META')}}
+   select * from stg_pems__station_meta
+), 
+
+station_meta_with_date AS (
+    select 
+    date_from_parts(
+        SUBSTR(filename, 18, 4)::INT,
+        SUBSTR(filename, 23, 2)::INT,
+        SUBSTR(RIGHT(filename,6), 1, 2)::INT
+    ) as meta_date, 
+    id, 
+    fwy as freeway, 
+    dir as direction,
+    district,
+    county as county_fips,
+    city,
+    state_pm as state_postmile,
+    abs_pm as absolute_postmile, 
+    latitude, 
+    longititude, 
+    length,
+    type,
+    lanes,
+    name
+    from source
+)
+
+
+
 select *
-from stg_pems__station_meta
+from station_meta_with_date
 limit 10
