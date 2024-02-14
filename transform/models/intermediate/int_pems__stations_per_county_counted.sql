3001b560021b67a3991512f7241a0e9bc2e20b42
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-02-14 23:37:41 +0000

    Created intermediate table with station count by county in 2023 and update models.yml

--- transform/models/intermediate/int_pems__stations_per_county_counted.sql
+++ transform/models/intermediate/int_pems__stations_per_county_counted.sql
@@ -1,3 +1,17 @@
-select *
-from stg_pems__station_meta
-limit 10
+with
+
+source as (
+    select * from {{ ref("stg_pems__station_meta") }}
+),
+
+stations_per_county as (
+    select
+        county,
+        COUNT(distinct id) as station_count
+    from source
+    where DATE_PART(year, meta_date)::INT = 2023
+    group by county
+)
+
+select * from stations_per_county
+order by station_count desc
