77232b18eb65ef7d0eabdb97281ae32a4220b190
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-02-16 23:06:42 +0000

    updated staging 30SecRaw.sql file to remove NULL issue

--- transform/models/staging/stg_pems__station_30SecRaw.sql
+++ transform/models/staging/stg_pems__station_30SecRaw.sql
@@ -2,35 +2,35 @@ select
     ID as STATION_ID,
     SAMPLE_TIMESTAMP,
     case
-        when FLOW_1 is not NULL and OCCUPANCY_1 is not NULL then 1
+        when FLOW_1 is not null and OCCUPANCY_1 is not null then 1
         else 0
     end as LANE_1_COUNT,
     case
-        when FLOW_2 is not NULL and OCCUPANCY_2 is not NULL then 1
+        when FLOW_2 is not null and OCCUPANCY_2 is not null then 1
         else 0
     end as LANE_2_COUNT,
     case
-        when FLOW_3 is not NULL and OCCUPANCY_3 is not NULL then 1
+        when FLOW_3 is not null and OCCUPANCY_3 is not null then 1
         else 0
     end as LANE_3_COUNT,
     case
-        when FLOW_4 is not NULL and OCCUPANCY_4 is not NULL then 1
+        when FLOW_4 is not null and OCCUPANCY_4 is not null then 1
         else 0
     end as LANE_4_COUNT,
     case
-        when FLOW_5 is not NULL and OCCUPANCY_5 is not NULL then 1
+        when FLOW_5 is not null and OCCUPANCY_5 is not null then 1
         else 0
     end as LANE_5_COUNT,
     case
-        when FLOW_6 is not NULL and OCCUPANCY_6 is not NULL then 1
+        when FLOW_6 is not null and OCCUPANCY_6 is not null then 1
         else 0
     end as LANE_6_COUNT,
     case
-        when FLOW_7 is not NULL and OCCUPANCY_7 is not NULL then 1
+        when FLOW_7 is not null and OCCUPANCY_7 is not null then 1
         else 0
     end as LANE_7_COUNT,
     case
-        when FLOW_8 is not NULL and OCCUPANCY_8 is not NULL then 1
+        when FLOW_8 is not null and OCCUPANCY_8 is not null then 1
         else 0
     end as LANE_8_COUNT
 
