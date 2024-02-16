220c9bfcb73d8abad552c3315ddeb675ab4db5c9
Zifeng Wu <Zifeng.Wu@dot.ca.gov>
2024-02-16 20:02:35 +0000

    Build an intermediate view with station counts per county in 2023, added to _models.yml.

--- transform/models/intermediate/int_pems__stations_per_county_counted.sql
+++ transform/models/intermediate/int_pems__stations_per_county_counted.sql
@@ -1,3 +1,24 @@
+with
+
+source as (
+    select * from {{ ref("stg_pems__station_meta") }}
+),
+
+county_data as (
+    select
+        county,
+        Count(id) as sta_counts
+    from source
+    where Year(meta_date) = 2023
+    group by county
+
+)
+
 select *
-from stg_pems__station_meta
-limit 10
+from county_data
+order by sta_counts desc
+
+
+-- select *
+-- from stg_pems__station_meta
+-- limit 10
