b963d61a94c6aed38927e11a44f2cd3e79e2d8f2
pingping.xiu@dot.ca.gov <pingping.xiu@dot.ca.gov>
2024-02-16 23:11:22 +0000

    test

--- transform/models/intermediate/int_station_id_new.sql
+++ transform/models/intermediate/int_station_id_new.sql
@@ -1,7 +1,7 @@
 with
 source as (
     select * from {{ source('CLEARINGHOUSE', 'STATION_RAW') }}
-   -- select * from stg_pems__station_meta
+-- select * from stg_pems__station_meta
 )
 
 select * from source
