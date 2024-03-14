0bfe409d7dc5bc022d3f02e7ac5aace052f687b6
Britt Allen <britt.allen@innovation.ca.gov>
2024-03-14 20:10:24 +0000

    materialized this model as incremental

--- transform/models/intermediate/int_pems__too_few_samples_test.sql
+++ transform/models/intermediate/int_pems__too_few_samples_test.sql
@@ -1,12 +1,17 @@
--- # of samples < 60% of the max collected samples during the test period.
--- max value is a static value, which should be two samples per minute times 60 mins/hr times 17 hours in a day 
--- (1224 is the static number of samples) - 
--- this max value doesnt need to be calculated at the station_id level bc it's the same
--- btwn 1 and 1224 is too few samples.
+{{ config(
+        materialized='incremental',
+        unique_key=['station_id', 'sample_date']
+    )
+}}
 
 with
 diagnostic_samples_per_station as (
     select * from {{ ref("int_pems__diagnostic_samples_per_station") }}
+    {% if is_incremental() %}
+        where sample_date > (
+            select dateadd(day, -2, max(sample_date)) from {{ this }}
+        )
+    {% endif %}
 )
 
 select
@@ -20,6 +25,10 @@ select
     coalesce(lane6_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane6_too_few_samples,
     coalesce(lane7_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane7_too_few_samples,
     coalesce(lane8_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane8_too_few_samples
+
+    -- # of samples < 60% of the max collected samples during the test period
+    -- max value: 2 samples per minute times 60 mins/hr times 17 hours in a day which == 1224 
+    -- btwn 1 and 1224 is too few samples
 
 from diagnostic_samples_per_station
 group by all
