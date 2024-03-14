8b3c10ec87f179661770b3c6772621e89690f80d
Britt Allen <britt.allen@innovation.ca.gov>
2024-03-14 20:00:58 +0000

    query to assign a boolean true value to lanes where there are too few samples otherwise false

--- transform/models/intermediate/int_pems__too_few_samples_test.sql
+++ transform/models/intermediate/int_pems__too_few_samples_test.sql
@@ -7,79 +7,19 @@
 with
 diagnostic_samples_per_station as (
     select * from {{ ref("int_pems__diagnostic_samples_per_station") }}
-),
-
-lane1_too_few_samples as (
-    select lane1_sample_cnt
-
-    from diagnostic_samples_per_station
-    where lane1_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
-),
-
-lane2_too_few_samples as (
-    select lane2_sample_cnt
-
-    from diagnostic_samples_per_station
-    where lane2_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
-),
-
-lane3_too_few_samples as (
-    select lane3_sample_cnt
-
-    from diagnostic_samples_per_station
-    where lane3_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
-),
-
-lane4_too_few_samples as (
-    select lane4_sample_cnt
-
-    from diagnostic_samples_per_station
-    where lane4_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
-),
-
-lane5_too_few_samples as (
-    select lane5_sample_cnt
-
-    from diagnostic_samples_per_station
-    where lane5_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
-),
-
-lane6_too_few_samples as (
-    select lane6_sample_cnt
-
-    from diagnostic_samples_per_station
-    where lane6_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
 )
 
--- , lane7_too_few_samples as (
---     select lane7_sample_cnt
-
---     from diagnostic_samples_per_station
---     where lane7_sample_cnt between 1 and (0.6 * (2 * 60 * 17))
--- )
-
--- , lane8_too_few_samples as (
--- select LANE8_SAMPLE_CNT
-
--- from diagnostic_samples_per_station
--- where LANE8_SAMPLE_CNT between 1 and (0.6*(2*60*17))
--- )
-
 select
-    lane1_too_few_samples.*,
-    lane2_too_few_samples.*,
-    lane3_too_few_samples.*,
-    lane4_too_few_samples.*,
-    lane5_too_few_samples.*,
-    lane6_too_few_samples.*
--- , lane7_too_few_samples.* 
--- , lane8_too_few_samples.*  -- currently lane 7 and lane 8 causes the query to produce no results!
+    sample_date,
+    station_id,
+    coalesce(lane1_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane1_too_few_samples,
+    coalesce(lane2_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane2_too_few_samples,
+    coalesce(lane3_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane3_too_few_samples,
+    coalesce(lane4_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane4_too_few_samples,
+    coalesce(lane5_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane5_too_few_samples,
+    coalesce(lane6_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane6_too_few_samples,
+    coalesce(lane7_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane7_too_few_samples,
+    coalesce(lane8_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane8_too_few_samples
 
-from lane1_too_few_samples,
-    lane2_too_few_samples,
-    lane3_too_few_samples,
-    lane4_too_few_samples,
-    lane5_too_few_samples,
-    lane6_too_few_samples
--- , lane7_too_few_samples 
--- , lane8_too_few_samples -- currently lane 7 and lane 8 causes the query to produce no results!
+from diagnostic_samples_per_station
+group by all
