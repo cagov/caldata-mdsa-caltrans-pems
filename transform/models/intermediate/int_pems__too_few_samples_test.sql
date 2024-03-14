5cccb581d9674a60171e444e307438ff9503ce2b
Britt Allen <britt.allen@innovation.ca.gov>
2024-03-14 20:20:10 +0000

    removed trailing whitespace and moved comment to be before the lines of code it references

--- transform/models/intermediate/int_pems__too_few_samples_test.sql
+++ transform/models/intermediate/int_pems__too_few_samples_test.sql
@@ -17,6 +17,9 @@ diagnostic_samples_per_station as (
 select
     sample_date,
     station_id,
+    -- # of samples < 60% of the max collected samples during the test period
+    -- max value: 2 samples per minute times 60 mins/hr times 17 hours in a day which == 1224
+    -- btwn 1 and 1224 is too few samples
     coalesce(lane1_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane1_too_few_samples,
     coalesce(lane2_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane2_too_few_samples,
     coalesce(lane3_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane3_too_few_samples,
@@ -25,10 +28,6 @@ select
     coalesce(lane6_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane6_too_few_samples,
     coalesce(lane7_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane7_too_few_samples,
     coalesce(lane8_sample_cnt between 1 and (0.6 * (2 * 60 * 17)), false) as lane8_too_few_samples
-
-    -- # of samples < 60% of the max collected samples during the test period
-    -- max value: 2 samples per minute times 60 mins/hr times 17 hours in a day which == 1224 
-    -- btwn 1 and 1224 is too few samples
 
 from diagnostic_samples_per_station
 group by all
