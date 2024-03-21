687ae252a379298192f39cf9885f55b502d764d9
Ian Rose <ian.rose@innovation.ca.gov>
2024-03-21 11:37:09 -0700

    Don't join with detector metadata at this stage, it's expensive without much benefit

--- transform/models/intermediate/clearinghouse/int_clearinghouse__station_raw.sql
+++ transform/models/intermediate/clearinghouse/int_clearinghouse__station_raw.sql
@@ -4,37 +4,10 @@ with station_raw as (
     select * from {{ ref('stg_clearinghouse__station_raw') }}
 ),
 
-station_status as (
-    select
-        station_id,
-        detector_id,
-        -- could do log10 or string parsing here, but perhaps less clear
-        case lane_number
-            when 1
-                then 1
-            when 10
-                then 2
-            when 100
-                then 3
-            when 1000
-                then 4
-            when 10000
-                then 5
-            when 100000
-                then 6
-            when 1000000
-                then 7
-            when 10000000
-                then 8
-        end as lane_number,
-        _valid_from,
-        _valid_to
-    from {{ ref('int_clearinghouse__station_status') }}
-),
-
 station_raw_unpivot as (
     select
         s.id as station_id,
+        s.district,
         s.sample_date,
         s.sample_timestamp,
         lane.value as lane,
@@ -67,19 +40,6 @@ station_raw_unpivot as (
     from station_raw as s,
         lateral flatten([1, 2, 3, 4, 5, 6, 7, 8]) as lane
     where s.sample_date = '2024-01-01'
-),
-
-station_raw_with_detector as (
-    select
-        station_status.detector_id,
-        station_raw_unpivot.*
-    from station_raw_unpivot
-    left join station_status
-        on
-            station_raw_unpivot.station_id = station_status.station_id
-            and station_raw_unpivot.lane = station_status.lane_number
-            and station_raw_unpivot.sample_date >= station_status._valid_from
-            and station_raw_unpivot.sample_date < coalesce(station_status._valid_to, dateadd('day', 1, current_date()))
 )
 
-select * from station_raw_with_detector
+select sum(flow) as flow from station_raw_unpivot
