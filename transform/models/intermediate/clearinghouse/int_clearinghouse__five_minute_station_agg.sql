4a4bad4109ca3e5cd53a0005612a9aa211762228
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-03-26 15:45:19 +0000

    Updated speed calculation based on unpivoted data.

--- transform/models/intermediate/clearinghouse/int_clearinghouse__five_minute_station_agg.sql
+++ transform/models/intermediate/clearinghouse/int_clearinghouse__five_minute_station_agg.sql
@@ -21,13 +21,13 @@ with station_raw as (
         ) as sample_timestamp_trunc
     from {{ ref('stg_clearinghouse__station_raw') }}
     where sample_date >= dateadd(year, -1, current_date())
-/*    {% if is_incremental() %}
+    /*{% if is_incremental() %}
         -- Look back two days to account for any late-arriving data
         where sample_date > (
             select dateadd(day, -2, max(sample_date)) from {{ this }}
         )
     {% endif %}
-*/
+    */
 ),
 
 aggregated as (
@@ -39,7 +39,6 @@ aggregated as (
         sum(volume) as volume, -- Sum of all the flow values
         avg(occupancy) as occupancy -- Average of all the occupancy values
     from station_raw
-<<<<<<< HEAD:transform/models/intermediate/vds/int_vds__five_minute_station_agg.sql
     group by id, sample_date, sample_timestamp_trunc
 ),
 
@@ -54,47 +53,11 @@ aggregated_speed as (
         --a conversion to get miles per hour (5280 ft / mile and 12
         --5-minute intervals in an hour).
         case
-            when flow_1 = 0 or occupancy_1 = 0 then 0
-            when flow_1 is null or occupancy_1 is null then null
-            --else (flow_1 * 22) / occupancy_1 / 5280 * 12
-            else (flow_1) / occupancy_1 / 600
-        end as speed_1,
-        case
-            when flow_2 = 0 or occupancy_2 = 0 then 0
-            when flow_2 is null or occupancy_2 is null then null
-            else (flow_2 * 22) / occupancy_2 / 5280 * 12
-        end as speed_2,
-        case
-            when flow_3 = 0 or occupancy_3 = 0 then 0
-            when flow_3 is null or occupancy_3 is null then null
-            else (flow_3 * 22) / occupancy_3 / 5280 * 12
-        end as speed_3,
-        case
-            when flow_4 = 0 or occupancy_4 = 0 then 0
-            when flow_4 is null or occupancy_4 is null then null
-            else (flow_4 * 22) / occupancy_4 / 5280 * 12
-        end as speed_4,
-        case
-            when flow_5 = 0 or occupancy_5 = 0 then 0
-            when flow_5 is null or occupancy_5 is null then null
-            else (flow_5 * 22) / occupancy_5 / 5280 * 12
-        end as speed_5,
-        case
-            when flow_6 = 0 or occupancy_6 = 0 then 0
-            when flow_6 is null or occupancy_6 is null then null
-            else (flow_6 * 22) / occupancy_6 / 5280 * 12
-        end as speed_6,
-        case
-            when flow_7 = 0 or occupancy_7 = 0 then 0
-            when flow_7 is null or occupancy_7 is null then null
-            else (flow_7 * 22) / occupancy_7 / 5280 * 12
-        end as speed_7,
-        case
-            when flow_8 = 0 or occupancy_8 = 0 then 0
-            when flow_8 is null or occupancy_8 is null then null
-            else (flow_8 * 22) / occupancy_8 / 5280 * 12
-        end as speed_8
-
+            when volume = 0 or occupancy = 0 then 0
+            when volume is null or occupancy is null then null
+            else (volume * 22) / occupancy / 5280 * 12
+            --else (volume) / occupancy / 600
+        end as speed
     from aggregated
     group by id, lane, sample_date, sample_timestamp_trunc
 )
