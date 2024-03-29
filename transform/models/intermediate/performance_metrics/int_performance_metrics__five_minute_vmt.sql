7c157ca3171591fa4316fbcecc6b113e4a9da489
zhenyu zhu <zhenyu.zhu@dot.ca.gov>
2024-03-29 01:56:10 +0000

    running code, but seems to need optimization.

--- transform/models/intermediate/performance_metrics/int_performance_metrics__five_minute_vmt.sql
+++ transform/models/intermediate/performance_metrics/int_performance_metrics__five_minute_vmt.sql
@@ -3,29 +3,29 @@
     cluster_by=["sample_date"],
     unique_key=["ID", "LANE", "SAMPLE_TIMESTAMP"],
     snowflake_warehouse=get_snowflake_refresh_warehouse()
-) }}
+) 
+}}
 
 with station_meta as (
     select
-        ID,
-        LENGTH,
-        TYPE,
-        LANES,
-        META_DATE,
-        _VALID_FROM,
-        _VALID_TO
+        id,
+        length,
+        type,
+        lanes,
+        meta_date,
+        _valid_from,
+        _valid_to
     from {{ ref('int_clearinghouse__station_meta') }}
-    WHERE TYPE = "ML"
-
+    where type = 'ML'
 ),
 
 fivemin_agg as (
-    SELECT 
+    select
         lane,
         sample_date,
         sample_timestamp,
         volume
-    FROM {{ ref('int_clearinghouse__five_minute_station_agg') }}
+    from {{ ref('int_clearinghouse__five_minute_station_agg') }}
 
     {% if is_incremental() %}
         -- Look back two days to account for any late-arriving data
@@ -33,23 +33,22 @@ fivemin_agg as (
             select dateadd(day, -2, max(sample_date)) from {{ this }}
         )
     {% endif %}
-)
-
+),
 
 vmt_agg_5min as (
     select
-        station_meta.ID,
+        station_meta.id,
         fivemin_agg.lane,
         fivemin_agg.sample_date,
         fivemin_agg.sample_timestamp,
         -- VMT calculation (5 min agg volume * station coverage length)
-        fivemin_agg.volume * station_meta.LENGTH as VMT 
-    from  fivemin_agg 
+        fivemin_agg.volume * station_meta.length as vmt
+    from fivemin_agg
     left join station_meta
-        where station_meta.ID = fivemin_agg.ID 
-            and station_meta.Lane = fivemin_agg.lane
-    group by id, lane, sample_date, sample_timestamp
-
+        on
+            fivemin_agg.id = station_meta.id
+            and fivemin_agg.lane = station_meta.lanes
+    group by station_meta.id, fivemin_agg.lane, fivemin_agg.sample_date, fivemin_agg.sample_timestamp
 )
 
-select * from vmt_agg_5min
+select * from vmt_agg_5min;
