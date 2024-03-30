0d4479025a2d6865d1c39663acf3cf909d88a9b3
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-03-30 00:55:17 +0000

    First draft of detector status table where additional QA/QC is needed

---
+++ transform/models/intermediate/detector_health_diag/int_pems__detector_status.sql
@@ -0,0 +1,60 @@
+{{ config(
+    materialized="incremental",
+    cluster_by=['sample_date'],
+    unique_key=['station_id', 'sample_date', 'lane'],
+    snowflake_warehouse="transforming_xl_dev"
+) }}
+
+with
+det_diag_sample_count as (
+    select * from {{ ref ('int_pems__diagnostic_samples_per_station') }}
+    where sample_date <= CURRENT_DATE
+        --TO_TIME(sample_timestamp) >= '05:00:00' and TO_TIME(sample_timestamp) <= '21:59:59'
+        {% if is_incremental() %}
+            -- Look back two days to account for any late-arriving data
+            and sample_date > (
+                select DATEADD(day, -2, MAX(sample_date)) from {{ this }}
+            )
+        {% endif %}
+        {% if target.name != 'prd' %}
+            and sample_date >= DATEADD('day', -14, CURRENT_DATE())
+        {% endif %}
+),
+
+detector_status as (
+    select
+        ddsc.*,
+        case
+            when ddsc.sample_ct = 0 or ddsc.sample_ct is null then 'Down/No Data'
+            -- # of samples < 60% of the max collected samples during the test period
+            -- max value: 2 samples per minute times 60 mins/hr times 17 hours in a day which == 1224
+            -- btwn 1 and 1224 is too few samples
+            when ddsc.sample_ct between 1 and (0.6 * (2 * 60 * 17)) then 'Insufficient Data'
+            when
+                set_assgnmt.station_diagnostic_method_id = 'ramp'
+                and ddsc.zero_vol_ct / (2 * 60 * 17) > (set_assgnmt.zero_flow_percent / 100) then 'Card Off'
+            when
+                set_assgnmt.station_diagnostic_method_id = 'mainline'
+                and ddsc.zero_occ_ct / (2 * 60 * 17) > (set_assgnmt.zero_occupancy_percent / 100) then 'Card Off'
+            when
+                set_assgnmt.station_diagnostic_method_id = 'ramp'
+                and ddsc.high_volume_ct / (2 * 60 * 17) > (set_assgnmt.high_flow_percent / 100) then 'High Val'
+            when
+                set_assgnmt.station_diagnostic_method_id = 'mainline'
+                and ddsc.high_occupancy_ct / (2 * 60 * 17) > (set_assgnmt.high_occupancy_percent / 100) then 'High Val'
+            when
+                set_assgnmt.station_diagnostic_method_id = 'mainline'
+                and ddsc.zero_vol_pos_occ_ct / (2 * 60 * 17) > (set_assgnmt.flow_occupancy_percent / 100) then 'Intermittent'
+            when
+                set_assgnmt.station_diagnostic_method_id = 'mainline'
+                and ddsc.zero_occ_pos_vol_ct / (2 * 60 * 17) > (set_assgnmt.occupancy_flow_percent / 100) then 'Intermittent'
+            --constant occupancy case needed
+            --Feed unstable case needed
+            else 'Good'
+        end as status
+    from det_diag_sample_count as ddsc
+    left join {{ ref('int_pems__det_diag_set_assignment') }} as set_assgnmt
+        on ddsc.station_id = set_assgnmt.station_id
+)
+
+select * from detector_status
--- transform/models/intermediate/detector_health_diag/int_pems__diagnostic_samples_per_station.sql
+++ transform/models/intermediate/detector_health_diag/int_pems__diagnostic_samples_per_station.sql
@@ -2,7 +2,7 @@
     materialized="incremental",
     cluster_by=['sample_date'],
     unique_key=['station_id', 'sample_date', 'lane'],
-    snowflake_warehouse=get_snowflake_refresh_warehouse(small="XL")
+    snowflake_warehouse="transforming_xl_dev"
 ) }}
 
 with
@@ -10,15 +10,15 @@ source as (
     select * from {{ ref ('stg_clearinghouse__station_raw') }}
     where
         TO_TIME(sample_timestamp) >= '05:00:00' and TO_TIME(sample_timestamp) <= '21:59:59'
-        {% if is_incremental() %}
+    {% if is_incremental() %}
             -- Look back two days to account for any late-arriving data
             and sample_date > (
                 select DATEADD(day, -2, MAX(sample_date)) from {{ this }}
             )
-        {% endif %}
-        {% if target.name == 'dev' %}
+    {% endif %}
+    {% if target.name != 'prd' %}
             and sample_date >= DATEADD('day', -14, CURRENT_DATE())
-        {% endif %}
+    {% endif %}
 ),
 
 samples_per_station as (
@@ -70,17 +70,6 @@ samples_per_station as (
         on source.id = set_assgnmt.station_id
 
     group by source.id, source.sample_date, source.lane
-),
-
-det_diag_too_few_samples as (
-    select
-        *,
-        -- # of samples < 60% of the max collected samples during the test period
-        -- max value: 2 samples per minute times 60 mins/hr times 17 hours in a day which == 1224
-        -- btwn 1 and 1224 is too few samples
-        COALESCE(sample_ct between 1 and (0.6 * (2 * 60 * 17)), false) as too_few_samples
-
-    from samples_per_station
 )
 
-select * from det_diag_too_few_samples
+select * from samples_per_station
