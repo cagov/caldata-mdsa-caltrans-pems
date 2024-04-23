634ef3b4bf3c6f9ce5591a6a08cd75390e4ad1b1
britt <47340362+traintestbritt@users.noreply.github.com>
2024-04-23 13:49:08 -0700

    introducing a third set of problems

--- transform/models/intermediate/detector_health_diag/int_pems__detector_status.sql
+++ transform/models/intermediate/detector_health_diag/int_pems__detector_status.sql
@@ -1,38 +1,41 @@
 {{ config(
     materialized="table"
 ) }}
-
-detector_status as (
+samples_per_station as (
+ select * from {{ ref('int_pems__det_diag_set_assignment') }}
+ where sample_date > year(2023)
+)
+, detector_status as (
     select
         sps.*,
         case
             when sps.sample_ct = 0 or sps.sample_ct is null
-                then 'Down_or_No_Data'
+                then 'Down/No Data'
             /* # of samples < 60% of the max collected during the test period
             max value: 2 samples per min * 60 mins/hr * 17 hrs in a day == 1224
             btwn 1 and 1224 is too few samples */
             when sps.sample_ct between 1 and (0.6 * ({{ var("detector_status_max_sample_value") }}))
-                then 'Insufficient_Data'
+                then 'Insufficient Data'
             when
                 set_assgnmt.station_diagnostic_method_id = 'ramp'
                 and sps.zero_vol_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.zero_flow_percent / 100)
-                then 'Card_Off'
+                then 'Card Off'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
                 and sps.zero_occ_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.zero_occupancy_percent / 100)
-                then 'Card_Off'
+                then 'Card Off'
             when
                 set_assgnmt.station_diagnostic_method_id = 'ramp'
                 and sps.high_volume_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.high_flow_percent / 100)
-                then 'High_Value'
+                then 'High Val'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
                 and sps.high_occupancy_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.high_occupancy_percent / 100)
-                then 'High_Value'
+                then 'High Val'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
                 and sps.zero_vol_pos_occ_ct / ({{ var("detector_status_max_sample_value") }})
@@ -45,7 +48,7 @@ detector_status as (
                 then 'Intermittent'
             else 'Superb'
         end as status
-    from {{ ref('int_pems__det_diag_set_assignment') }} as set_assgnmt
+    from samples_per_station as set_assgnmt
     left join {{ ref ('int_pems__diagnostic_samples_per_station') }} as sps
         on
             set_assgnmt.station_id = sps.station_id
