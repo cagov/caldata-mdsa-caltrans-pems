aea26267dadd665da19678aa04db4c2595f0d00e
Britt Allen <britt.allen@innovation.ca.gov>
2024-04-25 23:14:23 +0000

    began calculating the delay performance metric

--- transform/models/intermediate/performance/int_performance__five_min_perform_metrics.sql
+++ transform/models/intermediate/performance/int_performance__five_min_perform_metrics.sql
@@ -5,6 +5,8 @@
     snowflake_warehouse = get_snowflake_refresh_warehouse(small="XL")
 ) }}
 
+{% set V_t_list = var('V_t') %}
+
 with
 
 five_minute_agg as (
@@ -62,6 +64,41 @@ vmt_vht_metrics as (
         vmt / nullifzero(vht) as q_value,
         60 / nullifzero(q_value) as tti
     from five_minute_agg_with_station_meta
+),
+
+delay_metrics as (
+    select
+        *,
+
+        /* The Delay performance metric is the amount of additional time spent
+        by the vehicles on a section of road due to congestion. This is the
+        difference between the travel time at a non-congestion speed and the
+        current speed. The congestion, or threshold, speed is usually 35MPH but
+        in PeMS we compute the delay for a number of different thresholds so that
+        we can accommodate different definitions of delay. The threshold values
+        used are 35, 40, 45, 50, 55, and 60 MPH. The formula is: D = F * (TT - TT_t) = F * (L/V - L/V_t).
+        D = delay (can never be negative)
+        TT = the travel time (unclear how this is calculated or where it comes from)
+        TT_t = the travel time at the threshold speed (unclear how this is calculated or where it comes from)
+        F = the flow
+        L = the length of the segment
+        V = the current speed
+        V_t = the threshold speed */
+
+        flow * (60 -) as delay_one,
+        flow * (length / speed - length /{{ var("V_t") }}) as delay_two
+
+
+    from vmt_vht_metrics
+    where v_t in (
+        {% for code in V_t_list %}
+            {% if not loop.last %}
+                {{ code }},
+            {% else %}
+                {{ code }}
+            {% endif %}
+        {% endfor %}
+    )
 )
 
-select * from vmt_vht_metrics
+select * from delay_metrics
