60da22264a268f1f2b438051e1ced6f8f9d748aa
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-05-01 20:51:14 +0000

    updated weighted speed and along with five minutes imputed missing speed

--- transform/models/intermediate/clearinghouse/int_clearinghouse__five_minute_station_agg.sql
+++ transform/models/intermediate/clearinghouse/int_clearinghouse__five_minute_station_agg.sql
@@ -51,12 +51,15 @@ aggregated as (
         sample_date,
         sample_timestamp_trunc as sample_timestamp,
         lane,
+        --Number of raw data samples
         count_if(volume is not null and occupancy is not null)
-            as sample_ct, --Number of raw data samples
-        sum(volume) as volume, -- Sum of all the flow values
-        avg(occupancy) as occupancy -- Average of all the occupancy values
-        -- avg(speed) as speed_raw -- This code could be used if we use
-        -- actual speeds reported from raw data
+            as sample_ct,
+        -- Sum of all the flow values 
+        sum(volume) as volume,
+        -- Average of all the occupancy values 
+        avg(occupancy) as occupancy,
+        -- calculate_weighted_speed
+        sum(volume * speed) / nullifzero(sum(volume)) as weighted_speed
     from station_raw
     group by id, lane, sample_date, sample_timestamp_trunc
 ),
@@ -64,6 +67,7 @@ aggregated as (
 aggregated_speed as (
     select
         *,
+
         --A preliminary speed calcuation was developed on 3/22/24
         --using a vehicle effective length of 22 feet
         --(16 ft vehicle + 6 ft detector zone) feet and using
@@ -72,8 +76,8 @@ aggregated_speed as (
         --The following code may be used if we want to use speed from raw data
         --coalesce(speed_raw, ((volume * 22) / nullifzero(occupancy)
         --* (1 / 5280) * 12))
-        --    as speed
-        (volume * 22) / nullifzero(occupancy) * (1 / 5280) * 12 as speed
+        --impute five minutes missing speed
+        coalesce(weighted_speed, (volume * 22) / nullifzero(occupancy) * (1 / 5280) * 12) as imputed_speed
     from aggregated
 )
 
