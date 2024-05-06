4cc8148c5ea7707c7badb85b8d8482edf03970b1
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-05-06 21:36:42 +0000

    modified CET aggregation and also removed highway category

--- transform/models/intermediate/clearinghouse/int_cleaninghouse_global_nearby_station.sql
+++ transform/models/intermediate/clearinghouse/int_cleaninghouse_global_nearby_station.sql
@@ -2,7 +2,7 @@ with station_meta as (
     select * from {{ ref('int_clearinghouse__most_recent_station_meta') }}
 ),
 
-station_pairs as (
+nearest_stations_within_five_miles as (
     select
         a.id,
         b.id as other_id,
@@ -19,25 +19,12 @@ station_pairs as (
     -- TODO: distance comparisons don't seem appropriate for all station types,
     -- e.g., on/off ramps. So it makes sense to restrict these comparisons to some
     -- types. Is this the right set?
-    where a.type in ('HV', 'ML', 'CH', 'CD')
-),
-
-nearest_stations_within_five_miles as (
-    select *
-    from station_pairs
-    -- select the stations those are within 5 miles upstream and downstream of the station
-    where delta_postmile >= -5.0 and delta_postmile <= 5.0 and id != other_id
+    where a.type in ('HV', 'ML') and abs(delta_postmile) <= 5.0 and a.id != b.id
 ),
 
 nearest_stations_within_five_miles_count as (
     select
-        id,
-        other_id,
-        district,
-        freeway,
-        direction,
-        type,
-        delta_postmile,
+        *,
         count(*) over (partition by id) as other_id_count
     from nearest_stations_within_five_miles
 )
