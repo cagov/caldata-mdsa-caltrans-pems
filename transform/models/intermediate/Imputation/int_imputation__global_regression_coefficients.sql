4ead487397f83b1369d8ac5bb47af9c9a6b31f0d
Md Mintu Miah <166866681+mmmiah@users.noreply.github.com>
2024-05-06 14:47:51 -0700

    Update int_imputation__global_regression_coefficients.sql

--- transform/models/intermediate/Imputation/int_imputation__global_regression_coefficients.sql
+++ transform/models/intermediate/Imputation/int_imputation__global_regression_coefficients.sql
@@ -1,12 +1,1 @@
-{{ config(materialized="table") }}
 
--- station_status as (
---     select 
---         detector_id,
---         station_id,
---         district,
---         len(lane_number) as lane
---     from {{ ref('int_clearinghouse__most_recent_station_status') }}
--- )
-
-select * from {{ ref('int_clearinghouse__most_recent_station_status') }}
