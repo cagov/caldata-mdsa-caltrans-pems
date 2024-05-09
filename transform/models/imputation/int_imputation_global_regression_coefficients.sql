8365f46d475731fb098dc8674094554514ab27a5
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-05-09 20:55:03 +0000

    incorporated speed model

--- transform/models/imputation/int_imputation_global_regression_coefficients.sql
+++ transform/models/imputation/int_imputation_global_regression_coefficients.sql
@@ -12,7 +12,7 @@ with station_status as (
 ),
 
 station_counts as (
-    select * from {{ ref('int_clearinghouse__five_minute_station_agg') }}
+    select * from {{ ref('int_performance__five_min_perform_metrics') }}
     where sample_date = '2024-05-05'::date
 ),
 
@@ -41,6 +41,8 @@ station_counts_pairwise as (
         a.sample_timestamp,
         a.lane,
         b.lane as other_lane,
+        a.speed_five_mins as speed,
+        b.speed_five_mins as other_speed,
         a.volume_sum as volume,
         b.volume_sum as other_volume,
         a.occupancy_avg as occupancy,
@@ -59,7 +61,7 @@ station_counts_pairwise as (
 cleaned_model_data as (
     select *
     from station_counts_pairwise
-    where volume is not null and other_volume is not null and occupancy is not null and other_occupancy is not null
+    where (volume is not null and other_volume is not null) and (occupancy is not null and other_occupancy is not null) and (speed is not null and other_speed is not null)
 ),
 
 station_counts_regression_model as (
@@ -69,8 +71,13 @@ station_counts_regression_model as (
         sample_date,
         lane,
         other_id,
+        -- speed regression model
+        regr_slope(speed, other_speed) as speed_slope,
+        regr_intercept(speed, other_speed) as speed_intercept,
+        -- flow or volume regression model
         regr_slope(volume, other_volume) as volume_slope,
         regr_intercept(volume, other_volume) as volume_intercept,
+        -- occupancy regression model
         regr_slope(occupancy, other_occupancy) as occupancy_slope,
         regr_intercept(occupancy, other_occupancy) as occupancy_intercept
     from cleaned_model_data
