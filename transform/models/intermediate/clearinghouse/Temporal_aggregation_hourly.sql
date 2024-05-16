c009c19d179dc9a52035a2a0a5b3b95861d461e3
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-05-16 21:26:18 +0000

    temporal_hourly_aggregation

--- transform/models/intermediate/clearinghouse/Temporal_aggregation_hourly.sql
+++ transform/models/intermediate/clearinghouse/Temporal_aggregation_hourly.sql
@@ -1,5 +1,36 @@
-{{config(materialized='table')}}
+{{ config(materialized='table') }}
 
 -- read the volume, occupancy and speed five minutes data
-with station_flow as {{ ref('int_performance__five_min_perform_metrics') }}
+with station_five_mins_data as (select * from {{ ref('int_performance__five_min_perform_metrics') }}),
 
+-- separate the hour from the sample_timestamp
+hour_extract as (
+    select
+        id,
+        sample_date,
+        sample_timestamp,
+        lane,
+        volume_sum,
+        occupancy_avg,
+        speed_five_mins,
+        type,
+        extract(hour from sample_timestamp) as sample_hour
+    from station_five_mins_data
+
+),
+
+-- now aggregate hourly volume, occupancy and speed
+hourly_temporal_metrics as (
+    select
+        id,
+        sample_date,
+        lane,
+        sample_hour,
+        sum(volume_sum) as hourly_volume,
+        avg(occupancy_avg) as hourly_occupancy,
+        sum(volume_sum * speed_five_mins) / nullifzero(sum(volume_sum)) as hourly_speed
+    from hour_extract
+    group by id, sample_date, sample_hour, lane
+)
+
+select * from hourly_temporal_metrics
