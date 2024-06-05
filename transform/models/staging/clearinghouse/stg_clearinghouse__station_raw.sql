b2cea61230d0ae8e6c6d49972e723f9d548e1dd9
Ian Rose <ian.rose@innovation.ca.gov>
2024-06-05 20:32:45 +0000

    Revert lateral flatten for the moment

--- transform/models/staging/clearinghouse/stg_clearinghouse__station_raw.sql
+++ transform/models/staging/clearinghouse/stg_clearinghouse__station_raw.sql
@@ -5,45 +5,29 @@ select
     s.id,
     s.sample_date,
     s.sample_timestamp,
-    lane.value as lane,
-    -- neat trick to flatten multiple columns at once rather than using UNPIVOT:
-    -- https://stackoverflow.com/questions/36798558/lateral-flatten-two-columns-without-repetition-in-snowflake
-    [
-        s.flow_1,
-        s.flow_2,
-        s.flow_3,
-        s.flow_4,
-        s.flow_5,
-        s.flow_6,
-        s.flow_7,
-        s.flow_8
-    ][
-        lane.index
-    ] as volume,
-    [
-        s.occupancy_1,
-        s.occupancy_2,
-        s.occupancy_3,
-        s.occupancy_4,
-        s.occupancy_5,
-        s.occupancy_6,
-        s.occupancy_7,
-        s.occupancy_8
-    ][
-        lane.index
-    ] as occupancy,
-    [
-        s.speed_1,
-        s.speed_2,
-        s.speed_3,
-        s.speed_4,
-        s.speed_5,
-        s.speed_6,
-        s.speed_7,
-        s.speed_8
-    ][
-        lane.index
-    ] as speed
+    s.flow_1,
+    s.flow_2,
+    s.flow_3,
+    s.flow_4,
+    s.flow_5,
+    s.flow_6,
+    s.flow_7,
+    s.flow_8,
+    s.occupancy_1,
+    s.occupancy_2,
+    s.occupancy_3,
+    s.occupancy_4,
+    s.occupancy_5,
+    s.occupancy_6,
+    s.occupancy_7,
+    s.occupancy_8,
+    s.speed_1,
+    s.speed_2,
+    s.speed_3,
+    s.speed_4,
+    s.speed_5,
+    s.speed_6,
+    s.speed_7,
+    s.speed_8,
 from {{ source('clearinghouse', 'station_raw') }} as s,
-    lateral flatten([1, 2, 3, 4, 5, 6, 7, 8]) as lane
 where s.sample_date >= {{ var("pems_clearinghouse_start_date") }}
