19c3b88eb30615a1937d5889443dc70d1298dc4e
Ian Rose <ian.rose@innovation.ca.gov>
2024-06-07 14:01:15 -0700

    Implement manual unpivot using union all

--- transform/models/intermediate/db96/int_db96__vds30sec_long.sql
+++ transform/models/intermediate/db96/int_db96__vds30sec_long.sql
@@ -1,4 +1,8 @@
-{{ config(materialized="table") }}
+{{ config(
+    materialized="table",
+    cluster_by="sample_date",
+    snowflake_warehouse="transforming_xl_dev",
+) }}
 {% set n_lanes = 14 %}
 
 with raw as (
@@ -6,36 +10,26 @@ with raw as (
     from {{ ref("stg_db96__vds30sec") }}
 ),
 
-raw_long as (
-    select
-        raw.id,
-        raw.sample_date,
-        raw.sample_timestamp,
-        raw.district,
-        lane.value as lane,
-        [
-            {% for lane in range(1, n_lanes+1) %}
-                raw.flow_{{ lane }}{{ "," if not loop.last }}
-            {% endfor %}
-        ][
-            lane.index
-        ] as flow,
-        [
-            {% for lane in range(1, n_lanes+1) %}
-                raw.occupancy_{{ lane }}{{ "," if not loop.last }}
-            {% endfor %}
-        ][
-            lane.index
-        ] as occupancy,
-        [
-            {% for lane in range(1, n_lanes+1) %}
-                raw.speed_{{ lane }}{{ "," if not loop.last }}
-            {% endfor %}
-        ][
-            lane.index
-        ] as speed
-    from raw,
-        lateral flatten([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]) as lane
+{% for lane in range(1, n_lanes+1) %}
+    raw_{{ lane }} as (
+        select
+            raw.id,
+            raw.sample_date,
+            raw.sample_timestamp,
+            raw.district,
+            {{ lane }} as lane,
+            raw.flow_{{ lane }} as flow,
+            raw.occupancy_{{ lane }} as occupancy,
+            raw.speed_{{ lane }} as speed
+        from raw
+    ),
+{% endfor %}
+
+unioned as (
+    {% for lane in range(1, n_lanes+1) %}
+        select * from raw_{{ lane }}
+        {{ "union all" if not loop.last }}
+    {% endfor %}
 )
 
-select * from raw_long
+select * from unioned
