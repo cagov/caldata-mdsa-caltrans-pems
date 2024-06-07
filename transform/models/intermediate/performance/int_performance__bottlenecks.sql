85c93bc700c03575c9d04493d217d69e35edc2c0
Britt Allen <britt.allen@innovation.ca.gov>
2024-06-07 21:18:16 +0000

    fixed incorrect speed drop value, materializing model incrementally

--- transform/models/intermediate/performance/int_performance__bottlenecks.sql
+++ transform/models/intermediate/performance/int_performance__bottlenecks.sql
@@ -1,4 +1,9 @@
-{{ config(materialized="table") }}
+{{ config(
+    materialized="incremental",
+    cluster_by=["sample_date"],
+    unique_key=["id", "sample_timestamp"],
+    snowflake_warehouse = get_snowflake_refresh_warehouse(small="XL")
+) }}
 
 with
 
@@ -7,11 +12,28 @@ five_minute_pm as (
         id,
         sample_date,
         sample_timestamp,
-        speed_five_mins,
+        speed_five_mins,  -- will need to update, this is speed agg'd at the lane level and we need it at the station level
         delay_60_mph,
         coalesce(speed_five_mins < 40, false) as speed_less_than_40
 
     from {{ ref ("int_performance__five_min_perform_metrics") }}
+        {% if is_incremental() %}
+            -- Look back two days to account for any late-arriving data
+            and sample_date > (
+                select
+                    DATEADD(
+                        day,
+                        {{ var("incremental_model_look_back") }},
+                        MAX(sample_date)
+                    )
+                from {{ this }}
+            )
+        {% endif %}
+        {% if target.name != 'prd' %}
+            and sample_date
+            >= dateadd('day', {{ var("dev_model_look_back") }}, current_date())
+        {% endif %}
+
 ),
 
 distance as (
@@ -38,6 +60,17 @@ calculate_speed_delta as (
             over (partition by sample_timestamp, freeway, direction, type order by absolute_postmile asc)
             as speed_delta
     from five_minute_agg_with_distance
+
+    -- right now this is look at speed drop between adjacent stations
+    -- we want to look at speed drops between the start of the bottleneck and the end
+    -- find the initial 20mph drop (when the bottleneck starts), not a constant drop (maybe ranking??) 
+    -- e.g i am going 60 then next data point is 40 mph - trigger that as the start. then to identify the 
+    -- rest of the bottleneck check if still going 40mph or less for 5 out of 7 data points
+    -- delta of 20 and less than 40 at that station is the start, look at the previous 5min am i still going 
+    -- 40 and still within 3 miles that captures the length
+    -- check that their still going less than or equal to speed at start of bottleneck as soon as they go over that speed 
+    -- e.g. 40 mph they are out of the bottleneck
+    -- aggregate bottleneck over space and time to get extent (length) and duration
 ),
 
 bottleneck_criteria_check as (
@@ -47,7 +80,7 @@ bottleneck_criteria_check as (
             when
                 speed_less_than_40 = true
                 and distance_delta < 3
-                and speed_delta > 19
+                and speed_delta <= -20
                 then 1
             else 0
         end as bottleneck
