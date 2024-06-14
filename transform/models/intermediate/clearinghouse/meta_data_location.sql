0cc9b5da74b670910d1539e648a5951a25c4cde3
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-06-14 23:14:26 +0000

    meta_location

---
+++ transform/models/intermediate/clearinghouse/meta_data_location.sql
@@ -0,0 +1,23 @@
+{{ config(materialized="table") }}
+
+with meta_location as (
+    select distinct
+        id,
+        direction,
+        city,
+        county,
+        district,
+        freeway,
+        lanes,
+        latitude,
+        longitude,
+        type
+    from {{ ref('stg_clearinghouse__station_meta') }}
+    where
+        meta_date >= '2023-06-10'
+        and meta_date <= '2024-06-09'
+        and district not in (1, 2, 6, 9)
+        and type in ('HV', 'ML')
+)
+
+select * from meta_location
