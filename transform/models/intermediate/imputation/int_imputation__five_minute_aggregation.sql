0d7b161a9300c2d74d3ccfb68e721ea7b4ba5c4e
Md Mintu Miah <166866681+mmmiah@users.noreply.github.com>
2024-06-17 15:10:52 -0700

    Update transform/models/intermediate/imputation/int_imputation__five_minute_aggregation.sql
    Co-authored-by: Ian Rose <ian.rose@innovation.ca.gov>

--- transform/models/intermediate/imputation/int_imputation__five_minute_aggregation.sql
+++ transform/models/intermediate/imputation/int_imputation__five_minute_aggregation.sql
@@ -113,7 +113,7 @@ samples_requiring_imputation_with_coeffs as (
     asof join coeffs  -- noqa
         match_condition(samples_requiring_imputation.sample_date >= coeffs.regression_date)  -- noqa
         on samples_requiring_imputation.id = coeffs.id
-    where other_station_is_local = true
+    where coeffs.other_station_is_local = true
 ),
 
 -- Read the neighbours that have volume, occupancy and speed data.
