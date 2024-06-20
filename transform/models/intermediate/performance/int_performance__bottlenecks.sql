b5d9a1e0a9cb1da223d917d2314d0991cafe3bea
Britt Allen <britt.allen@innovation.ca.gov>
2024-06-20 20:07:49 +0000

    unsure of the changes made

--- transform/models/intermediate/performance/int_performance__bottlenecks.sql
+++ transform/models/intermediate/performance/int_performance__bottlenecks.sql
@@ -61,28 +61,25 @@ five_minute_agg_with_distance as (
             and d.id = f.id
 ),
 
-calculate_speed_delta as (
+speed_calcs as (
     select
         *,
-        speed_five_mins
-        - lag(speed_five_mins)
-            over (partition by sample_timestamp, freeway, direction, type order by absolute_postmile asc)
+        iff(direction = 'N' or direction = 'E', 
+        lag(speed_five_mins)
+            over (partition by sample_timestamp, freeway, direction, type order by sample_timestamp, freeway, direction, type, absolute_postmile asc), 
+        lag(speed_five_mins)
+            over (partition by sample_timestamp, freeway, direction, type order by sample_timestamp, freeway, direction, type, absolute_postmile desc))
+            as speed_prev,
+        iff(direction = 'N' or direction = 'E',
+        speed_five_mins - lag(speed_five_mins)
+            over (partition by sample_timestamp, freeway, direction, type order by sample_timestamp, freeway, direction, type, absolute_postmile asc), 
+        speed_five_mins - lag(speed_five_mins)
+            over (partition by sample_timestamp, freeway, direction, type order by sample_timestamp, freeway, direction, type, absolute_postmile desc))
             as speed_delta
-    from five_minute_agg_with_distance
-
-    -- right now this is look at speed drop between adjacent stations
-    -- we want to look at speed drops between the start of the bottleneck and the end
-    -- find the initial 20mph drop (when the bottleneck starts), not a constant drop (maybe ranking??) 
-    -- e.g i am going 60 then next data point is 40 mph - trigger that as the start. then to identify the 
-    -- rest of the bottleneck check if still going 40mph or less for 5 out of 7 data points
-    -- delta of 20 and less than 40 at that station is the start, look at the previous 5min am i still going 
-    -- 40 and still within 3 miles that captures the length
-    -- check that their still going less than or equal to speed at start of bottleneck as soon as they go over that speed 
-    -- e.g. 40 mph they are out of the bottleneck
-    -- aggregate bottleneck over space and time to get extent (length) and duration
+    from five_minute_agg_with_distance    
 ),
 
-bottleneck_start_check as (
+bottleneck_start_check_ne as (
     select
         *,
         case
@@ -90,56 +87,48 @@ bottleneck_start_check as (
                 speed_five_mins < 40
                 and distance_delta < 3
                 and speed_delta <= -20
+                and speed_prev >= 40
                 then 1
             else 0
         end as bottleneck_start
-    from calculate_speed_delta
-    qualify lag(speed_five_mins) over (
-        partition by sample_timestamp, freeway, direction, type
-        order by sample_timestamp asc, absolute_postmile asc
-    ) >= 40
+    from speed_calcs
+    where direction = 'N' or direction = 'E'
+    order by sample_timestamp, freeway, direction, type, absolute_postmile asc
 ),
 
-bottleneck_persists_check as (
+bottleneck_start_check_sw as (
     select
         *,
         case
             when
                 speed_five_mins < 40
                 and distance_delta < 3
+                and speed_delta <= -20
+                and speed_prev >= 40
                 then 1
             else 0
-        end as bottleneck
-    from bottleneck_start_check
-    qualify sum(bottleneck)
-        over (
-            partition by sample_timestamp, freeway, direction, type
-            order by absolute_postmile asc rows between 6 preceding and current row
-        )
-    >= 5 and sum(bottleneck_start)
-        over (
-            partition by sample_timestamp, freeway, direction, type
-            order by absolute_postmile asc rows between 6 preceding and current row
-        )
-    = 1
+        end as bottleneck_start
+    from speed_calcs
+    where direction = 'S' or direction = 'W'
+    order by sample_timestamp, freeway, direction, type, absolute_postmile desc
+),
+
+unioned_data as (
+select * from bottleneck_start_check_ne
+union all
+select * from bottleneck_start_check_sw
+), 
+
+bottleneck_persists_check as (
+    select
+        *,
+        sum(bottleneck_start) over (
+            partition by sample_timestamp, freeway, direction, type order by sample_timestamp asc rows between current row and 6 following
+            ) as bottleneck_sum
+    from unioned_data
 )
-
--- ,bottleneck_end_check as (
---     select
---         *,
---         case
---             when
---                 speed_five_mins < 40
---                 and distance_delta < 3
---                 then 1
---             else 0
---         end as bottleneck_end
---     from calculate_speed_delta
---     -- i also need to figure out if 5 of the 7 rows before this bottleneck end row were labeled 1
---     qualify lead(speed_five_mins) over (
---         partition by sample_timestamp, freeway, direction, type
---         order by sample_timestamp asc, absolute_postmile asc
---     ) >= 40
--- ) 
-
-select * from bottleneck_persists_check
+        
+select 
+    *,
+    iff(bottleneck_sum >= 5, true, false) as is_bottleneck
+ from bottleneck_persists_check
