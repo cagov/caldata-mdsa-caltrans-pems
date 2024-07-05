42014075c832d5419ec9057f0c55f199d083d5c3
Taheer Rahim <taheer.rahim@innovation.ca.gov>
2024-07-05 07:47:31 +0000

    Updated to work with new 5 min model

--- transform/models/intermediate/diagnostics/int_diagnostics__samples_per_station.sql
+++ transform/models/intermediate/diagnostics/int_diagnostics__samples_per_station.sql
@@ -1,3 +1,10 @@
+{{ config(
+    materialized="incremental",
+    cluster_by=['sample_date'],
+    unique_key=['station_id', 'sample_date', 'lane'],
+    snowflake_warehouse=get_snowflake_refresh_warehouse(small="XL")
+) }}
+
 with
 
 source as (
@@ -11,41 +18,41 @@ source as (
 
 samples_per_station as (
     select
-        district,
-        id as station_id,
-        lane,
-        sample_date,
+        source.district,
+        source.id as station_id,
+        source.lane,
+        source.sample_date,
         /*
         This following counts a sample if the volume (flow) and occupancy values contain any value
         based on 30 second raw data recieved per station, lane and time. Null values
         in volume (flow) and occupancy are currently counted as 0 but if these need to be treated
         differently the code should be updated as needed to accomodate such a scenario.
         */
-        sample_ct,
+        SUM(source.sample_ct) as sample_ct,
 
         /*
         The following code will count how many times a 30 second raw volume (flow) value equals 0
         for a given station and associated lane
         */
-        zero_vol_ct,
+        SUM(source.zero_vol_ct) as zero_vol_ct,
 
         /*
         The following code will count how many times a 30 second raw occupancy value equals 0
         for a given station and associated lane
         */
-        zero_occ_ct,
+        SUM(source.zero_occ_ct) as zero_occ_ct,
 
         /*
         This code counts a sample if the volume (flow) is 0 and occupancy value > 0
         based on 30 second raw data recieved per station, lane, and time.
         */
-        zero_vol_pos_occ_ct,
+        SUM(zero_vol_pos_occ_ct) as zero_vol_pos_occ_ct,
 
         /*
         This code counts a sample if the occupancy is 0 and a volume (flow) value > 0
         based on 30 second raw data recieved per station, lane and time.
         */
-        zero_occ_pos_vol_ct,
+        SUM(zero_occ_pos_vol_ct) as zero_occ_pos_vol_ct,
 
         /*
         This SQL file counts the number of volume (flow) and occupancy values that exceed
@@ -53,11 +60,14 @@ samples_per_station as (
         processing optimization a high flow value or 20 and high occupancy value of 0.7
         have been hardcoded in the formulas below to avoid joining the set assignment model
         */
-        high_volume_ct,
-        high_occupancy_ct
+        SUM(high_volume_ct)
+            as high_volume_ct,
+        SUM(high_occupancy_ct)
+            as high_occupancy_ct
 
     from source
-
+    group by
+        source.district, source.id, source.lane, source.sample_date
 )
 
 select * from samples_per_station
