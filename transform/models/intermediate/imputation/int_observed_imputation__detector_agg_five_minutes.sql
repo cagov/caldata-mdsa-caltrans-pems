5f8c3d659a889780116ac60ab0985c63361737dd
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-07-12 21:20:25 +0000

    included the imputation method and date tagging

--- transform/models/intermediate/imputation/int_observed_imputation__detector_agg_five_minutes.sql
+++ transform/models/intermediate/imputation/int_observed_imputation__detector_agg_five_minutes.sql
@@ -37,7 +37,7 @@ imputed_five_minutes_agg as (
 ),
 
 -- now join observed and imputed five mins agg
-obserbed_and_imputed_five_mins_agg as (
+observed_and_imputed_five_mins_agg as (
     select
         observed_five_minutes_agg.*,
         imputed_five_minutes_agg.* exclude (id, lane, sample_date, sample_timestamp, station_type)
@@ -54,7 +54,7 @@ obserbed_and_imputed_five_mins_agg as (
 -- tag if it is observed or imputed
 hybrid_five_mins_agg as (
     select
-        obserbed_and_imputed_five_mins_agg.* exclude (
+        observed_and_imputed_five_mins_agg.* exclude (
             volume_sum, speed_weighted, occupancy_avg,
             local_regression_date,
             regional_regression_date,
@@ -74,18 +74,56 @@ hybrid_five_mins_agg as (
         case
             when station_type in ('ML', 'HV') then not coalesce(volume_sum is not null, false)
         end as volume_sum_imputed,
+        case
+            when volume_sum is not null then 'detector_reported'
+            when volume_local_regression is not null then 'local'
+            when volume_regional_regression is not null then 'regional'
+            when volume_global_regression is not null then 'global'
+            else 'none'
+        end as volume_imputation_method,
+        case
+            when volume_sum is not null then null
+            when volume_local_regression is not null then local_regression_date
+            when volume_regional_regression is not null then regional_regression_date
+            when volume_global_regression is not null then global_regression_date
+        end as volume_imputation_date,
         coalesce(speed_weighted, speed_local_regression, speed_regional_regression, speed_global_regression)
             as speed_weighted,
         case
             when station_type in ('ML', 'HV') then not coalesce(speed_weighted is not null, false)
         end as speed_weighted_imputed,
-        coalesce(
-            occupancy_avg, occupancy_local_regression, occupancy_regional_regression, occupancy_global_regression
-        ) as occupancy_avg,
+        case
+            when speed_weighted is not null then 'detector_reported'
+            when speed_local_regression is not null then 'local'
+            when speed_regional_regression is not null then 'regional'
+            when speed_global_regression is not null then 'global'
+            else 'none'
+        end as speed_imputation_method,
+        case
+            when speed_weighted is not null then null
+            when speed_local_regression is not null then local_regression_date
+            when speed_regional_regression is not null then regional_regression_date
+            when speed_global_regression is not null then global_regression_date
+        end as speed_imputation_date,
+        coalesce(occupancy_avg, occupancy_local_regression, occupancy_regional_regression, occupancy_global_regression)
+            as occupancy_avg,
         case
             when station_type in ('ML', 'HV') then not coalesce(occupancy_avg is not null, false)
-        end as occupancy_avg_imputed
-    from obserbed_and_imputed_five_mins_agg
+        end as occupancy_avg_imputed,
+        case
+            when occupancy_avg is not null then 'detector_reported'
+            when occupancy_local_regression is not null then 'local'
+            when occupancy_regional_regression is not null then 'regional'
+            when occupancy_global_regression is not null then 'global'
+            else 'none'
+        end as occupancy_imputation_method,
+        case
+            when occupancy_avg is not null then null
+            when occupancy_local_regression is not null then local_regression_date
+            when occupancy_regional_regression is not null then regional_regression_date
+            when occupancy_global_regression is not null then global_regression_date
+        end as occupancy_imputation_date
+    from observed_and_imputed_five_mins_agg
 )
 
 -- select the final observed and imputed five mins agg table
