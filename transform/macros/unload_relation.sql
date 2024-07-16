0fcbff6aa54f9db55c36ee5f88337ad28338abcd
Ian Rose <ian.rose@innovation.ca.gov>
2024-07-16 16:33:11 -0700

    work on partitioned unload

--- transform/macros/unload_relation.sql
+++ transform/macros/unload_relation.sql
@@ -4,17 +4,22 @@
   {% else %}
     {% set suffix = 'DEV' %}
   {% endif %}
+  {% set partitioning = config.get('unload_partitioning', '') %}
   {% set stage = '@ANALYTICS_' ~ suffix ~ '.PUBLIC.PEMS_MARTS_' ~ suffix %}
-  {% set object_name = model.name.split('_')[2:] | reject("eq", "") | join("_")  %}
-  {% set object_name = object_name ~ '.parquet' if config.get('unload_single', True) else '' %}
-  {% set path = this.schema ~ '/' ~ object_name %}
+  {% set key = model.name.split('_')[2:] | reject("eq", "") | join("_")  %}
+  {% set key = key ~ ('' if partitioning else '.parquet') %}
+  {% set path = this.schema ~ '/' ~ key %}
   {% set url = stage ~ '/' ~ path %}
-  copy into {{ url }}
-  from {{ this }}
-  file_format = (type=parquet)
-  {% if config.get('unload_single', True) %}
-      single = true
-      max_file_size = 536870912 -- 512 MiB
-      overwrite = true
-  {% endif %}
+      remove {{ url }};
+      copy into {{ url }}
+      from {{ this }}
+      file_format = (type=parquet)
+      {% if partitioning %}
+          partition by {{ partitioning }}
+          max_file_size = 134217728 -- 128 MiB
+      {% else %}
+          single = true
+          max_file_size = 536870912 -- 512 MiB
+      {% endif %}
+      ;
 {% endmacro %}
