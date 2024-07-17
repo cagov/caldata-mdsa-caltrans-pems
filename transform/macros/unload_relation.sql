d16e8d822aa775de379bc681d9409a8d9678b1fb
Ian Rose <ian.rose@innovation.ca.gov>
2024-07-17 15:33:42 +0000

    Add the ability to strip a custom number of leading words. Add database/schema config for marts. Add a couple of demonstration marts

--- transform/macros/unload_relation.sql
+++ transform/macros/unload_relation.sql
@@ -1,12 +1,12 @@
-{% macro unload_relation() %}
+{% macro unload_relation(strip_leading_words=1, unload_partitioning=none) %}
   {% if target.name == 'prd' %}
     {% set suffix = 'PRD' %}
   {% else %}
     {% set suffix = 'DEV' %}
   {% endif %}
-  {% set partitioning = config.get('unload_partitioning', '') %}
+  {% set partitioning = unload_partitioning or config.get('unload_partitioning', none) %}
   {% set stage = '@ANALYTICS_' ~ suffix ~ '.PUBLIC.PEMS_MARTS_' ~ suffix %}
-  {% set key = model.name.split('_')[2:] | reject("eq", "") | join("_")  %}
+  {% set key = model.name.split('_')[strip_leading_words:] | reject("eq", "") | join("_")  %}
   {% set key = key ~ ('' if partitioning else '.parquet') %}
   {% set path = this.schema ~ '/' ~ key %}
   {% set url = stage ~ '/' ~ path %}
