47da3820b9828d240506c0d1f83a7ecbcdcea000
Ian Rose <ian.rose@innovation.ca.gov>
2024-07-17 08:49:37 -0700

    Pre-commit

--- transform/models/marts/performance/performance__station_metrics_agg_daily.sql
+++ transform/models/marts/performance/performance__station_metrics_agg_daily.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized="table",
     unload_partitioning="('year=' || to_varchar(date_part(year, sample_date)) || '/month=' || to_varchar(date_part(month, sample_date)))",
-)}}
+) }}
 
 with daily as (
     select * from {{ ref('int_performance__station_metrics_agg_daily') }}
