3d5bf4af0c665983b3c810b63fb1646fc9c3089d
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-07-23 00:40:43 +0000

    completed the model, now need to update ymal file

--- transform/models/marts/imputation/imputation__station_summary.sql
+++ transform/models/marts/imputation/imputation__station_summary.sql
@@ -16,14 +16,56 @@ imputation_count as (
         station_id,
         district,
         sample_date,
+        direction,
+        lane,
+        freeway,
         count(station_id) as sample_ct,
-        count(case when occupancy_imputation_method = 'local' then station_id end) as number_local_imputation,
-        count(case when occupancy_imputation_method = 'regional' then station_id end) as number_regional_imputation,
-        count(case when occupancy_imputation_method = 'global' then station_id end) as number_global_imputation,
-        count(case when occupancy_imputation_method = 'observed' then station_id end) as number_observed_sample,
-        count(case when occupancy_imputation_method is NULL then station_id end) as number_unobserved_unimputed
+        count(case when occupancy_imputation_method = 'local' then station_id end) as occ_local_imputation_sample,
+        count(case when occupancy_imputation_method = 'regional' then station_id end) as occ_regional_imputation_sample,
+        count(case when occupancy_imputation_method = 'global' then station_id end) as occ_global_imputation_sample,
+        count(case when occupancy_imputation_method = 'observed' then station_id end) as occ_observed_sample,
+        count(case when occupancy_imputation_method is NULL then station_id end) as occ_unobserved_unimputed,
+        count(case when volume_imputation_method = 'local' then station_id end) as vol_local_imputation_sample,
+        count(case when volume_imputation_method = 'regional' then station_id end) as vol_regional_imputation_sample,
+        count(case when volume_imputation_method = 'global' then station_id end) as vol_global_imputation_sample,
+        count(case when volume_imputation_method = 'observed' then station_id end) as vol_observed_sample,
+        count(case when volume_imputation_method is NULL then station_id end) as vol_unobserved_unimputed,
+        count(case when speed_imputation_method = 'local' then station_id end) as speed_local_imputation_sample,
+        count(case when speed_imputation_method = 'regional' then station_id end) as speed_regional_imputation_sample,
+        count(case when speed_imputation_method = 'global' then station_id end) as speed_global_imputation_sample,
+        count(case when speed_imputation_method = 'observed' then station_id end) as speed_observed_sample,
+        count(case when speed_imputation_method is NULL then station_id end) as speed_unobserved_unimputed
     from obs_imputed_five_minutes_agg
-    group by station_id, district, sample_date
+    group by station_id, district, lane, direction, freeway, sample_date
+),
+
+imputation_pct as (
+    select
+        station_id,
+        district,
+        sample_date,
+        direction,
+        lane,
+        freeway,
+        sample_ct,
+        (coalesce(occ_local_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_occupancy_local_regression,
+        (coalesce(occ_regional_imputation_sample, 0) / nullif(sample_ct, 0))
+        * 100 as pct_of_occupancy_regional_regression,
+        (coalesce(occ_global_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_occupancy_global_regression,
+        (coalesce(occ_unobserved_unimputed, 0) / nullif(sample_ct, 0)) * 100 as pct_of_occupancy_unobserved_unimputed,
+        (coalesce(occ_observed_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_occupancy_observed,
+        (coalesce(vol_local_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_volume_local_regression,
+        (coalesce(vol_regional_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_volume_regional_regression,
+        (coalesce(vol_global_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_volume_global_regression,
+        (coalesce(vol_unobserved_unimputed, 0) / nullif(sample_ct, 0)) * 100 as pct_of_volume_unobserved_unimputed,
+        (coalesce(vol_observed_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_volume_observed,
+        (coalesce(speed_local_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_speed_local_regression,
+        (coalesce(speed_regional_imputation_sample, 0) / nullif(sample_ct, 0))
+        * 100 as pct_of_speed_regional_regression,
+        (coalesce(speed_global_imputation_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_speed_global_regression,
+        (coalesce(speed_unobserved_unimputed, 0) / nullif(sample_ct, 0)) * 100 as pct_of_speed_unobserved_unimputed,
+        (coalesce(speed_observed_sample, 0) / nullif(sample_ct, 0)) * 100 as pct_of_speed_observed
+    from imputation_count
 )
 
-select * from imputation_count
+select * from imputation_pct
