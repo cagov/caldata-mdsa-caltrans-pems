0d7971d80aa09189e562e32e6e041a4386cb07ea
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-07-27 00:26:51 +0000

    wip, reviewing the output since the number of Good and Card Off detectors does not look correct

---
+++ transform/models/intermediate/clearinghouse/int_clearinghouse__active_detectors.sql
@@ -0,0 +1,31 @@
+with date_range as (
+        {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="to_date('01/01/2010', 'mm/dd/yyyy')",
+        end_date= "current_date + 1"
+        )
+        }}
+),
+
+date_range_updated as (
+    select date_{{ "day" }} as active_date from date_range
+),
+
+detector_meta as (
+    select * from {{ ref("int_vds__detector_config") }}
+),
+
+active_detector as (
+    select
+        dr.*,
+        dm.*
+    from date_range_updated as dr
+    inner join
+        detector_meta as dm
+        on
+            dr.active_date >= dm._valid_from
+            and (dr.active_date < dm._valid_to or dm._valid_to is null)
+    where dm.status = 1
+)
+
+select * from active_detector
--- transform/models/intermediate/diagnostics/int_diagnostics__detector_status.sql
+++ transform/models/intermediate/diagnostics/int_diagnostics__detector_status.sql
@@ -7,71 +7,93 @@
 
 with
 
-source as (
+source_samples as (
     select * from {{ ref('int_diagnostics__samples_per_detector') }}
     where {{ make_model_incremental('sample_date') }}
 ),
 
+source_detectors as (
+    select
+        active_date,
+        detector_id,
+        station_id,
+        lane,
+        detector_type,
+        station_type,
+        district
+    from {{ ref ('int_clearinghouse__active_detectors') }}
+),
+
+source_detector_samples as (
+    select
+        ss.* exclude (district, detector_id, station_id, lane),
+        sd.*
+    from source_detectors as sd
+    left join source_samples as ss
+        on
+            sd.detector_id = ss.detector_id
+            and sd.active_date = ss.sample_date
+),
+
 district_feed_check as (
     select
-        source.district,
-        source.sample_date,
+        source_samples.district,
+        source_samples.sample_date,
         case
-            when (count_if(source.sample_ct > 0)) > 0 then 'Yes'
+            when (count_if(source_samples.sample_ct > 0)) > 0 then 'Yes'
             else 'No'
         end as district_feed_working
-    from source
+    from source_samples
     inner join {{ ref('districts') }} as d
-        on source.district = d.district_id
-    group by source.district, source.sample_date
+        on source_samples.district = d.district_id
+    group by source_samples.district, source_samples.sample_date
 ),
 
 detector_status as (
     select
-        set_assgnmt.active_date,
+        sds.* exclude (district, station_id, station_type),
         set_assgnmt.station_id,
         set_assgnmt.district,
         set_assgnmt.station_type,
-        sps.* exclude (district, station_id),
         dfc.district_feed_working,
         co.min_occupancy_delta,
         case
             when dfc.district_feed_working = 'No' then 'District Feed Down'
-            when sps.sample_ct = 0 or sps.sample_ct is null
+            when sds.sample_ct = 0 or sds.sample_ct is null
                 then 'Down/No Data'
             /* # of samples < 60% of the max collected during the test period
             max value: 2 samples per min * 60 mins/hr * 17 hrs in a day == 1224
             btwn 1 and 1224 is too few samples */
-            when sps.sample_ct between 1 and (0.6 * ({{ var("detector_status_max_sample_value") }}))
+            when sds.sample_ct between 1 and (0.6 * ({{ var("detector_status_max_sample_value") }}))
                 then 'Insufficient Data'
             when
                 set_assgnmt.station_diagnostic_method_id = 'ramp'
-                and sps.zero_vol_ct / ({{ var("detector_status_max_sample_value") }})
+                and sds.zero_vol_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.zero_flow_percent / 100)
                 then 'Card Off'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
-                and sps.zero_occ_ct / ({{ var("detector_status_max_sample_value") }})
+                and sds.zero_occ_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.zero_occupancy_percent / 100)
                 then 'Card Off'
             when
                 set_assgnmt.station_diagnostic_method_id = 'ramp'
-                and sps.high_volume_ct / ({{ var("detector_status_max_sample_value") }})
+                and sds.high_volume_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.high_flow_percent / 100)
                 then 'High Val'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
-                and sps.high_occupancy_ct / ({{ var("detector_status_max_sample_value") }})
+                and sds.high_occupancy_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.high_occupancy_percent / 100)
                 then 'High Val'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
-                and sps.zero_vol_pos_occ_ct / ({{ var("detector_status_max_sample_value") }})
+                and sds.zero_vol_pos_occ_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.flow_occupancy_percent / 100)
                 then 'Intermittent'
             when
                 set_assgnmt.station_diagnostic_method_id = 'mainline'
-                and sps.zero_occ_pos_vol_ct / ({{ var("detector_status_max_sample_value") }})
+                and sds.zero_occ_pos_vol_ct / ({{ var("detector_status_max_sample_value") }})
                 > (set_assgnmt.occupancy_flow_percent / 100)
                 then 'Intermittent'
             when coalesce(co.min_occupancy_delta = 0, false)
@@ -80,15 +102,15 @@ detector_status as (
             else 'Good'
         end as status
 
-    from {{ ref('int_diagnostics__det_diag_set_assignment') }} as set_assgnmt
-    left join source as sps
+    from source_detector_samples as sds
+    left join {{ ref('int_diagnostics__det_diag_set_assignment') }} as set_assgnmt
         on
-            set_assgnmt.station_id = sps.station_id
-            and set_assgnmt.active_date = sps.sample_date
+            sds.station_id = set_assgnmt.station_id
+            and sds.sample_date = set_assgnmt.active_date
 
     left join {{ ref('int_diagnostics__constant_occupancy') }} as co
         on
-            sps.station_id = co.station_id and sps.lane = co.lane and sps.sample_date = co.sample_date
+            sds.station_id = co.station_id and sds.lane = co.lane and sds.sample_date = co.sample_date
     left join district_feed_check as dfc
         on
             set_assgnmt.district = dfc.district
