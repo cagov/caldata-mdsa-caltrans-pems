54097ebd2a717726ecb66464a6be0bb2002d3244
Taheer Rahim <taheer.rahim@innovation.ca.gov>
2024-08-05 20:31:41 +0000

    create scd macro

---
+++ transform/macros/get_scd_2_data.sql
@@ -0,0 +1,7 @@
+
+{% macro get_scd_2_data(active_date,_valid_from,_valid_to) -%}
+
+            {{ active_date }} >= {{ _valid_from }}
+            and ( {{ active_date }} < {{ _valid_to }} or {{ _valid_to }} is null)
+
+{% endmacro %}
