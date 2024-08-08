fd3bbc3c6cf5e2b4608384c408c14ae32ed98343
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-08-08 17:40:18 +0000

    revised sample

--- transform/models/marts/performance/detector_health_non_geospatial_sample.sql
+++ transform/models/marts/performance/detector_health_non_geospatial_sample.sql
@@ -7,9 +7,13 @@ WITH DETECTOR_HEALTH AS (
         D.STATION_TYPE AS DETECTOR_TYPE,
         D.LANE,
         D.SAMPLE_DATE,
-        D.STATUS
+        D.STATUS,
+        CASE
+            WHEN D.STATUS = 'Good' THEN 'Good'
+            ELSE 'Bad'
+        END AS DETECTOR_STATUS
     FROM {{ ref('int_diagnostics__detector_status') }} AS D
-    WHERE D.SAMPLE_DATE = '2024-07-18'
+    WHERE D.SAMPLE_DATE = DATEADD(DAY, -4, CAST(GETDATE() AS DATE))
 )
 
 SELECT * FROM DETECTOR_HEALTH
