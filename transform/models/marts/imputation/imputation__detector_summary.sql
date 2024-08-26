595a4349d2dfdee4bf995f88f0c730c035d33a51
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-08-26 18:30:06 +0000

    updated imputation upstream model

--- transform/models/marts/imputation/imputation__detector_summary.sql
+++ transform/models/marts/imputation/imputation__detector_summary.sql
@@ -20,16 +20,22 @@ imputation_count as (
         count_if(occupancy_imputation_method = 'local') as occ_local_imputation_sample,
         count_if(occupancy_imputation_method = 'regional') as occ_regional_imputation_sample,
         count_if(occupancy_imputation_method = 'global') as occ_global_imputation_sample,
+        count_if(occupancy_imputation_method = 'local_avg') as occ_local_avg_imputation_sample,
+        count_if(occupancy_imputation_method = 'regional_avg') as occ_regional_avg_imputation_sample,
         count_if(occupancy_imputation_method = 'observed') as occ_observed_sample,
         count_if(occupancy_imputation_method is NULL) as occ_unobserved_unimputed,
         count_if(volume_imputation_method = 'local') as vol_local_imputation_sample,
         count_if(volume_imputation_method = 'regional') as vol_regional_imputation_sample,
         count_if(volume_imputation_method = 'global') as vol_global_imputation_sample,
+        count_if(volume_imputation_method = 'local_avg') as vol_local_avg_imputation_sample,
+        count_if(volume_imputation_method = 'regional_avg') as vol_regional_avg_imputation_sample,
         count_if(volume_imputation_method = 'observed') as vol_observed_sample,
         count_if(volume_imputation_method is NULL) as vol_unobserved_unimputed,
         count_if(speed_imputation_method = 'local') as speed_local_imputation_sample,
         count_if(speed_imputation_method = 'regional') as speed_regional_imputation_sample,
         count_if(speed_imputation_method = 'global') as speed_global_imputation_sample,
+        count_if(speed_imputation_method = 'local_avg') as speed_local_avg_imputation_sample,
+        count_if(speed_imputation_method = 'regional_avg') as speed_regional_avg_imputation_sample,
         count_if(speed_imputation_method = 'observed') as speed_observed_sample,
         count_if(speed_imputation_method is NULL) as speed_unobserved_unimputed
     from obs_imputed_five_minutes_agg
@@ -47,17 +53,25 @@ imputation_pct as (
         coalesce(occ_regional_imputation_sample, 0) / nullifzero(sample_ct)
         * 100 as pct_of_occupancy_regional_regression,
         coalesce(occ_global_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_occupancy_global_regression,
+        coalesce(occ_local_avg_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_occupancy_local_avg,
+        coalesce(occ_regional_avg_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_occupancy_regional_avg,
         coalesce(occ_unobserved_unimputed, 0) / nullifzero(sample_ct) * 100 as pct_of_occupancy_unobserved_unimputed,
         coalesce(occ_observed_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_occupancy_observed,
+
         coalesce(vol_local_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_local_regression,
         coalesce(vol_regional_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_regional_regression,
         coalesce(vol_global_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_global_regression,
+        coalesce(vol_local_avg_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_local_avg,
+        coalesce(vol_regional_avg_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_regional_avg,
         coalesce(vol_unobserved_unimputed, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_unobserved_unimputed,
         coalesce(vol_observed_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_volume_observed,
+
         coalesce(speed_local_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_speed_local_regression,
         coalesce(speed_regional_imputation_sample, 0) / nullifzero(sample_ct)
         * 100 as pct_of_speed_regional_regression,
         coalesce(speed_global_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_speed_global_regression,
+        coalesce(speed_local_avg_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_speed_local_avg,
+        coalesce(speed_regional_avg_imputation_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_speed_regional_avg,
         coalesce(speed_unobserved_unimputed, 0) / nullifzero(sample_ct) * 100 as pct_of_speed_unobserved_unimputed,
         coalesce(speed_observed_sample, 0) / nullifzero(sample_ct) * 100 as pct_of_speed_observed
     from imputation_count
