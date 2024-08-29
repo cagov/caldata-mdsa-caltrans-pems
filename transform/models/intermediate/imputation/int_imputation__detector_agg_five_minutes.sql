cd771c34ec7de74d125744c5126e272d77b51287
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-08-29 16:42:18 +0000

    resolved  sqlfluff error

--- transform/models/intermediate/imputation/int_imputation__detector_agg_five_minutes.sql
+++ transform/models/intermediate/imputation/int_imputation__detector_agg_five_minutes.sql
@@ -32,7 +32,7 @@ with base as (
             when volume_sum = 0 and occupancy_avg = 0 then 0
             else speed_weighted
         end as speed_weighted
-    from {{ ref('int_clearinghouse__detector_agg_five_minutes_with_missing_rows')}}
+    from {{ ref('int_clearinghouse__detector_agg_five_minutes_with_missing_rows') }}
     where {{ make_model_incremental('sample_date') }}
 ),
 
