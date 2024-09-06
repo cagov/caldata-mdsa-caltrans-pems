e0e47f3100f166e3893bb152382cb1a01f3715c6
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-09-06 19:17:47 +0000

    Adder where statement to only include records in mart model where the sample date and lane are not null values

--- transform/models/marts/diagnostics/diagnostics__detector_daily_detail.sql
+++ transform/models/marts/diagnostics/diagnostics__detector_daily_detail.sql
@@ -7,6 +7,7 @@ with
 
 detector_status as (
     select * from {{ ref("int_diagnostics__detector_status") }}
+    where sample_date is not null and lane is not null
 )
 
 select * from detector_status
