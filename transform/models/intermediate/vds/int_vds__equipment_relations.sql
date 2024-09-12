b728cd29d93398793c37587a14112d92c3484063
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-09-12 15:45:10 +0000

    wip

---
+++ transform/models/intermediate/vds/int_vds__equipment_relations.sql
@@ -0,0 +1,39 @@
+with date_range as (
+        {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="to_date('01/01/2010', 'mm/dd/yyyy')",
+        end_date= "current_date + 1"
+        )
+        }}
+),
+
+date_range_updated as (
+    select date_{{ "day" }} as active_date from date_range
+),
+
+detector_meta as (
+    select * from {{ ref("int_vds__detector_config") }}
+),
+
+station_meta as (
+    select * from {{ ref("int_vds__station_config") }}
+),
+
+config_meta as (
+    select * from {{ ref ("int_vds__controller_config") }}
+),
+
+active_detector as (
+    select
+        dr.*,
+        dm.*
+    from date_range_updated as dr
+    inner join
+        detector_meta as dm
+        on
+            {{ get_scd_2_data('dr.active_date','dm._valid_from','dm._valid_to') }}
+
+    where dm.status = 1
+)
+
+select * from active_detector
