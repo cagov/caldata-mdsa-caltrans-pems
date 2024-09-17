5bb358b6b33b016d4084c8424a8c99e531d49dd6
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-09-17 23:10:31 +0000

    Update max_capacity model to use a consistent value of 173 vehicles/lane/5-minute for each detector's max capacity. Update station 5-minute aggregation model with performance metrics

--- transform/models/intermediate/performance/int_performance__max_capacity.sql
+++ transform/models/intermediate/performance/int_performance__max_capacity.sql
@@ -1,40 +1,35 @@
 /*
-This model develops the maximum capacity of a detector based on the historical measured
-raw data and determines the maximum observed 15-minute flow. We use the maximum of the
-actual 15-minute flow value and 2076 v/l/h as the capacity at each location. This will be used
-to determine the productivity performance metric.
+This model currently uses a maximum capacity of 2076 vehicles/lane/hour for all detectors.
+Using this value provides a consistent metric for determining the capcacity and
+calculating the lost productivity.
+
+The current methodology in PeMS uses the max of either the 15 minute historical highest
+flow or 2076 v/l/h as the capacity at each location per PeMS website:
+https://pems.dot.ca.gov/?dnode=Help&content=help_calc#perf
+
+The issue with the current methodology is that there is no documentation for how the historical
+measured maximum capacity has been developed and we have observed inconsistencies in high flow
+values in PeMS. We also do not have any documention on why a 15-minute timeframe was selected
+and how that value is used to compute the 5-minute aggregation level of the lost productivity
+performance metric.
 */
 
 with
 
 source as (
     select *
-    from {{ ref('int_imputation__detector_imputed_agg_five_minutes') }}
+    from {{ ref('int_vds__detector_config') }}
 ),
 
-sum_volume as (
-    select
-        *,
-        sum(volume_sum)
-        /* we are looking at a window of 3 rows because that is a 15-minute window
-        (5-min data * 3 = 15 minutes) */
-            over (
-                partition by detector_id, sample_date
-                order by sample_timestamp rows between 2 preceding and current row
-            )
-            as volume_summed
+max_capacity_detector as (
+    select distinct
+        detector_id,
+        /*
+        2076 v/l/h / 12 = 173 v/l/5-min
+        */
+        173 as max_capacity_5min
     from source
-    qualify volume_sum is not null
+    group by detector_id, station_id, physical_lanes
 )
 
-select
-    detector_id,
-    /*
-    Use max of 2076 v/l/h or 15 minute historical highest flow as the capacity
-    at each location per PeMS website:
-    https://pems.dot.ca.gov/?dnode=Help&content=help_calc#perf
-    2076 v/l/h / 12 = 173 v/l/5-min
-    */
-    greatest(max(volume_summed), 173) as max_capacity_5min
-from sum_volume
-group by all
+select * from max_capacity_detector
