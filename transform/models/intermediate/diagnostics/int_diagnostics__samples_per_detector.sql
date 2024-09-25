392b6415cb3008cb5d355d41bd6c27d46eb3f2f0
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-09-25 16:54:06 +0000

    updated sample_per_detector with on_schema_change call in config block

--- transform/models/intermediate/diagnostics/int_diagnostics__samples_per_detector.sql
+++ transform/models/intermediate/diagnostics/int_diagnostics__samples_per_detector.sql
@@ -2,6 +2,7 @@
     materialized="incremental",
     cluster_by=['sample_date'],
     unique_key=['station_id', 'sample_date', 'lane'],
+    on_schema_change='sync_all_columns',
     snowflake_warehouse=get_snowflake_refresh_warehouse(small="XL")
 ) }}
 
