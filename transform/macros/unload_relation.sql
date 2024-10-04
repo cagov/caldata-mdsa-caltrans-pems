47e82af7efb05457f21c32a47d49dd0b21f2a71f
zhenyu zhu <zhenyu.zhu@dot.ca.gov>
2024-10-04 20:37:20 +0000

    change the data unload sql to save data in csv instead of parquet format.

--- transform/macros/unload_relation.sql
+++ transform/macros/unload_relation.sql
@@ -6,8 +6,8 @@
   {% endif %}
   {% set partitioning = unload_partitioning or config.get('unload_partitioning', none) %}
   {% set stage = '@ANALYTICS_' ~ suffix ~ '.PUBLIC.PEMS_MARTS_' ~ suffix %}
-  {% set key = model.name.split('_')[strip_leading_words:] | reject("eq", "") | join("_")  %}
-  {% set key = key ~ ('' if partitioning else '.parquet') %}
+  {% set key = model.name.split('_')[strip_leading_words:] | reject("eq", "") | join("_") %}
+  {% set key = key ~ ('' if partitioning else '.csv') %}  {# Change file extension to .csv #}
   {% set path = this.schema ~ '/' ~ key %}
   {% set url = stage ~ '/' ~ path %}
       remove {{ url }};
@@ -16,7 +16,7 @@
       {% if partitioning %}
           partition by {{ partitioning }}
       {% endif %}
-      file_format = (type=parquet)
+      file_format = (type=csv, field_delimiter=',', skip_header=1)  {# Set file format to CSV with a delimiter and skip header #}
       {% if partitioning %}
           max_file_size = 134217728 -- 128 MiB
       {% else %}
@@ -25,3 +25,34 @@
       {% endif %}
       ;
 {% endmacro %}
+
+
+
+
+-- {% macro unload_relation(strip_leading_words=1, unload_partitioning=none) %}
+--   {% if target.name == 'prd' %}
+--     {% set suffix = 'PRD' %}
+--   {% else %}
+--     {% set suffix = 'DEV' %}
+--   {% endif %}
+--   {% set partitioning = unload_partitioning or config.get('unload_partitioning', none) %}
+--   {% set stage = '@ANALYTICS_' ~ suffix ~ '.PUBLIC.PEMS_MARTS_' ~ suffix %}
+--   {% set key = model.name.split('_')[strip_leading_words:] | reject("eq", "") | join("_")  %}
+--   {% set key = key ~ ('' if partitioning else '.parquet') %}
+--   {% set path = this.schema ~ '/' ~ key %}
+--   {% set url = stage ~ '/' ~ path %}
+--       remove {{ url }};
+--       copy into {{ url }}
+--       from {{ this }}
+--       {% if partitioning %}
+--           partition by {{ partitioning }}
+--       {% endif %}
+--       file_format = (type=parquet)
+--       {% if partitioning %}
+--           max_file_size = 134217728 -- 128 MiB
+--       {% else %}
+--           single = true
+--           max_file_size = 536870912 -- 512 MiB
+--       {% endif %}
+--       ;
+-- {% endmacro %}
