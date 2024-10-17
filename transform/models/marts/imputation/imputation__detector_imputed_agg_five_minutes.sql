43a34155388e226fc15eb77bf0a87b1ebf26f6b5
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-10-17 15:54:28 +0000

    limit the data for recent 4 days as the data size is big (5 mins data) and also limit the facility to HOV and ML as we imputed only for these facilities

--- transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
+++ transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
@@ -1,6 +1,9 @@
 with imputation_five_mins as (
     select *
     from {{ ref('int_imputation__detector_imputed_agg_five_minutes') }}
+    where
+        station_type in ('ML', 'HV')
+        and sample_date = dateadd(day, -4, current_date)
 )
 
 select * from imputation_five_mins
