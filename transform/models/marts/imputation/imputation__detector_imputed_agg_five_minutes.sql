cc2b4c53434e40b2589b494e2e510ce10646055b
Ken Godleski <124397677+kengodleskidot@users.noreply.github.com>
2024-10-21 09:48:59 -0700

    Update imputation__detector_imputed_agg_five_minutes.sql
    updated "=" to ">=" in where statement

--- transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
+++ transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
@@ -3,7 +3,7 @@ with imputation_five_mins as (
     from {{ ref('int_imputation__detector_imputed_agg_five_minutes') }}
     where
         station_type in ('ML', 'HV')
-        and sample_date = dateadd(day, -4, current_date)
+        and sample_date >= dateadd(day, -4, current_date)
 )
 
 select * from imputation_five_mins
