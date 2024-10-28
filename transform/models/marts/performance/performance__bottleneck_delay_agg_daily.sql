1be5262feffdb48e90097350f4303b613643f8c6
Hang Gao <Hang.Gao@dot.ca.gov>
2024-10-28 23:20:59 +0000

    fix county name issue with lower case

--- transform/models/marts/performance/performance__bottleneck_delay_agg_daily.sql
+++ transform/models/marts/performance/performance__bottleneck_delay_agg_daily.sql
@@ -1,23 +1,27 @@
 with daily_bottleneck_delay as (
-    select * from {{ ref('int_performance__bottleneck_delay_metrics_agg_daily') }}
-),
-
-county as (
     select
-        county_id,
-        county_name
-    from {{ ref('counties') }}
+        station_id,
+        sample_date,
+        time_shift,
+        cast(district as int) as district,
+        station_type,
+        freeway,
+        direction,
+        absolute_postmile,
+        daily_time_shift_duration,
+        daily_time_shift_bottleneck_extent,
+        daily_time_shift_spatial_delay_35_mph,
+        daily_time_shift_spatial_delay_40_mph,
+        daily_time_shift_spatial_delay_45_mph,
+        daily_time_shift_spatial_delay_50_mph,
+        daily_time_shift_spatial_delay_55_mph,
+        daily_time_shift_spatial_delay_60_mph,
+        county
+    from {{ ref('int_performance__bottleneck_delay_metrics_agg_daily') }}
 ),
 
 bottleneck_delay_with_county as (
-    select
-        daily_bottleneck_delay.* exclude (county),
-        c.county_name
-    from
-        daily_bottleneck_delay
-    inner join
-        county as c
-        on daily_bottleneck_delay.county = c.county_id
+    {{ get_county_name('daily_bottleneck_delay') }}
 ),
 
 geo as (
