817179f416090ec0434154fd87e4ef7e0bc627c3
Ken Godleski <kenneth.godleski@dot.ca.gov>
2024-10-30 23:09:08 +0000

    update code with correct spacing and tested ok

--- transform/tests/generic/five_minute_daily_count.sql
+++ transform/tests/generic/five_minute_daily_count.sql
@@ -1,7 +1,7 @@
 {% test five_minute_daily_count(model, select_columns, group_by_columns) %}
 with
 validation_errors as (
-    select{{ select_columns|join(',') }}
+    select {{ select_columns|join(',') }}
     from {{ model }}
     group by {{ group_by_columns|join(',') }}
     having count(*) != 288
