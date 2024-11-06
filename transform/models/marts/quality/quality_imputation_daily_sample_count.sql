622532d5d2c20dbb68fc5858dcd777cae351c6c6
Md Mintu Miah <Md.Mintu.Miah@dot.ca.gov>
2024-11-06 22:29:12 +0000

    added yml and removed the numerical test column

--- transform/models/marts/quality/quality_imputation_daily_sample_count.sql
+++ transform/models/marts/quality/quality_imputation_daily_sample_count.sql
@@ -21,25 +21,27 @@ imputation_status_count as (
         count_if(occupancy_imputation_method = 'local_avg') as occ_local_avg_imputation_sample,
         count_if(occupancy_imputation_method = 'regional_avg') as occ_regional_avg_imputation_sample,
         count_if(occupancy_imputation_method = 'observed') as occ_observed_sample,
-        count_if(occupancy_imputation_method is NULL) as occ_unobserved_unimputed,
+        count_if(occupancy_imputation_method = 'observed_unimputed') as occ_unobserved_unimputed,
         count_if(volume_imputation_method = 'local') as vol_local_imputation_sample,
         count_if(volume_imputation_method = 'regional') as vol_regional_imputation_sample,
         count_if(volume_imputation_method = 'global') as vol_global_imputation_sample,
         count_if(volume_imputation_method = 'local_avg') as vol_local_avg_imputation_sample,
         count_if(volume_imputation_method = 'regional_avg') as vol_regional_avg_imputation_sample,
         count_if(volume_imputation_method = 'observed') as vol_observed_sample,
-        count_if(volume_imputation_method is NULL) as vol_unobserved_unimputed,
+        count_if(volume_imputation_method = 'observed_unimputed') as vol_unobserved_unimputed,
         count_if(speed_imputation_method = 'local') as speed_local_imputation_sample,
         count_if(speed_imputation_method = 'regional') as speed_regional_imputation_sample,
         count_if(speed_imputation_method = 'global') as speed_global_imputation_sample,
         count_if(speed_imputation_method = 'local_avg') as speed_local_avg_imputation_sample,
         count_if(speed_imputation_method = 'regional_avg') as speed_regional_avg_imputation_sample,
         count_if(speed_imputation_method = 'observed') as speed_observed_sample,
-        count_if(speed_imputation_method is NULL) as speed_unobserved_unimputed,
-        count_if(occupancy_imputation_method != 'observed' and occupancy_imputation_method is not NULL)
+        count_if(speed_imputation_method = 'observed_unimputed') as speed_unobserved_unimputed,
+        count_if(occupancy_imputation_method != 'observed' and occupancy_imputation_method != 'observed_unimputed')
             as occ_imputed_sample,
-        count_if(volume_imputation_method != 'observed' and volume_imputation_method is not NULL) as vol_imputed_sample,
-        count_if(speed_imputation_method != 'observed' and speed_imputation_method is not NULL) as speed_imputed_sample
+        count_if(volume_imputation_method != 'observed' and volume_imputation_method != 'observed_unimputed')
+            as vol_imputed_sample,
+        count_if(speed_imputation_method != 'observed' and speed_imputation_method != 'observed_unimputed')
+            as speed_imputed_sample
     from obs_imputed_five_minutes_agg
     group by sample_date
 ),
@@ -49,33 +51,13 @@ sample_count as (
         *,
         (vol_imputed_sample / nullif(sample_ct, 0)) * 100 as pct_vol_imputed,
         (vol_observed_sample / nullif(sample_ct, 0)) * 100 as pct_vol_observed,
-        (vol_unobserved_unimputed / nullif(sample_ct, 0)) * 100 as pct_vol_unobserved,
+        (vol_unobserved_unimputed / nullif(sample_ct, 0)) * 100 as pct_vol_observed_unimputed,
         (speed_imputed_sample / nullif(sample_ct, 0)) * 100 as pct_speed_imputed,
         (speed_observed_sample / nullif(sample_ct, 0)) * 100 as pct_speed_observed,
-        (speed_unobserved_unimputed / nullif(sample_ct, 0)) * 100 as pct_speed_unobserved,
+        (speed_unobserved_unimputed / nullif(sample_ct, 0)) * 100 as pct_speed_observed_unimputed,
         (occ_imputed_sample / nullif(sample_ct, 0)) * 100 as pct_occ_imputed,
         (occ_observed_sample / nullif(sample_ct, 0)) * 100 as pct_occ_observed,
-        (occ_unobserved_unimputed / nullif(sample_ct, 0)) * 100 as pct_occ_unobserved,
-
-        -- Volume check: Sum of all volume percentages should equal 100
-        coalesce(
-            coalesce(pct_vol_imputed, 0) + coalesce(pct_vol_observed, 0) + coalesce(pct_vol_unobserved, 0) = 100,
-            FALSE
-        ) as volume_check_ok,
-
-        -- Speed check: Sum of all speed percentages should equal 100
-        coalesce(
-            coalesce(pct_speed_imputed, 0) + coalesce(pct_speed_observed, 0) + coalesce(pct_speed_unobserved, 0)
-            = 100,
-            FALSE
-        ) as speed_check_ok,
-
-        -- Occupancy check: Sum of all occupancy percentages should equal 100
-        coalesce(
-            coalesce(pct_occ_imputed, 0) + coalesce(pct_occ_observed, 0) + coalesce(pct_occ_unobserved, 0) = 100,
-            FALSE
-        ) as occ_check_ok
-
+        (occ_unobserved_unimputed / nullif(sample_ct, 0)) * 100 as pct_occ_observed_unimputed
     from imputation_status_count
 )
 
