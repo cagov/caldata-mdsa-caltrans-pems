e3c767ee162474c3a9cd13ac3da0747350a4b316
Summer Mothwood <summer.mothwood@innovation.ca.gov>
2024-11-08 20:22:47 +0000

    changing unique key config from 'station_id' + 'lane' to 'detector_id' for three detector-level models

--- transform/models/intermediate/diagnostics/int_diagnostics__samples_per_detector.sql
+++ transform/models/intermediate/diagnostics/int_diagnostics__samples_per_detector.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized="incremental",
     cluster_by=['sample_date'],
-    unique_key=['station_id', 'sample_date', 'lane'],
+    unique_key=['detector_id', 'sample_date'],
     on_schema_change='sync_all_columns',
     snowflake_warehouse=get_snowflake_refresh_warehouse(small="XL")
 ) }}
