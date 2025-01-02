ea1b09386b2a7c1566cbdf279cd4173a5f1d54a3
Hang Gao <Hang.Gao@dot.ca.gov>
2025-01-02 07:50:32 +0000

    update imputation_detector_metrics_agg_five_minutes by removing outliers for flow and occupancy

--- transform/models/intermediate/performance/int_performance__detector_metrics_agg_five_minutes.sql
+++ transform/models/intermediate/performance/int_performance__detector_metrics_agg_five_minutes.sql
@@ -35,6 +35,64 @@ five_minute_agg as (
     where {{ make_model_incremental('sample_date') }}
 ),
 
+/* Conduct monthly high flow and occupancy detection based on z-score and percentile, and correct outliers
+by imputing values based on the 95th percentile data
+*/
+
+monthly_stats as (
+    select
+        detector_id,
+        date_trunc('month', sample_date) as month,
+        avg(volume_sum) as volume_mean,
+        stddev(volume_sum) as volume_stddev,
+        -- consider using max_capacity
+        percentile_cont(0.95) within group (order by volume_sum) as volume_95th,
+        percentile_cont(0.95) within group (order by occupancy_avg) as occupancy_95th
+    from five_minute_agg
+    group by detector_id, date_trunc('month', sample_date)
+),
+
+outlier_removed_data as (
+    select
+        fa.*,
+        -- update volume_sum if it's an outlier
+        case
+            when
+                abs(fa.volume_sum - ms.volume_mean) / nullifzero(ms.volume_stddev) > 3
+                and fa.volume_imputation_method in ('observed', 'observed_unimputed')
+                then ms.volume_95th
+            else fa.volume_sum
+        end as volume_sum,
+        -- update volume sum imputation method
+        case
+            when
+                abs(fa.volume_sum - ms.volume_mean) / nullifzero(ms.volume_stddev) > 3
+                and fa.volume_imputation_method in ('observed', 'observed_unimputed')
+                then 'outlier'
+            else fa.volume_imputation_method
+        end as volume_imputation_method,
+        -- update occupancy if it's an outlier
+        case
+            when
+                fa.occupancy_avg > ms.occupancy_95th
+                and fa.occupancy_imputation_method in ('observed', 'observed_unimputed')
+                then ms.occupancy_95th
+            else fa.occupancy_avg
+        end as occupancy_avg,
+        case
+            when
+                fa.occupancy_avg > ms.occupancy_95th
+                and fa.occupancy_imputation_method in ('observed', 'observed_unimputed')
+                then 'outlier'
+            else fa.occupancy_imputation_method
+        end as occupancy_imputation_method
+    from five_minute_agg as fa
+    left join monthly_stats as ms
+        on
+            fa.detector_id = ms.detector_id
+            and date_trunc('month', fa.sample_date) = ms.month
+),
+
 vmt_vht_metrics as (
     select
         *,
@@ -46,7 +104,7 @@ vmt_vht_metrics as (
         vmt / nullifzero(vht) as q_value,
         -- travel time
         60 / nullifzero(q_value) as tti
-    from five_minute_agg
+    from outlier_removed_data
 ),
 
 delay_metrics as (
