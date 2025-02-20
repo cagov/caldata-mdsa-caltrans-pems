1e2c541cfa332f5b8f7afe811821467ee7795ae8
Ian Rose <ian.rose@innovation.ca.gov>
2025-02-20 09:03:46 -0800

    WIP create an iceberg materialization

--- transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
+++ transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
@@ -1,5 +1,7 @@
 with imputation_five_mins as (
-    select *
+    select
+        * exclude (sample_timestamp, station_valid_from, station_valid_to),
+        sample_timestamp::timestamp_ntz(6) as sample_timestamp
     from {{ ref('int_imputation__detector_imputed_agg_five_minutes') }}
     where
         station_type in ('ML', 'HV')
