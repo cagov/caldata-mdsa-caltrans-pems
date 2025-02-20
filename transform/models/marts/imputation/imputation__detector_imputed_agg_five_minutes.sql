58e0f3a4c5fe9200b03c8b91e93bc8c55c2ffbad
Ian Rose <ian.rose@innovation.ca.gov>
2025-02-20 14:12:11 -0800

    Incrementalize

--- transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
+++ transform/models/marts/imputation/imputation__detector_imputed_agg_five_minutes.sql
@@ -1,10 +1,11 @@
 with imputation_five_mins as (
     select
         * exclude (sample_timestamp, station_valid_from, station_valid_to),
-        sample_timestamp::timestamp_ntz(6) as sample_timestamp
+        sample_timestamp::timestamp_ntz(6) as sample_timestamp -- iceberg needs ms for timestamps
     from {{ ref('int_imputation__detector_imputed_agg_five_minutes') }}
     where
         station_type in ('ML', 'HV')
+        and {{ make_model_incremental('sample_date') }}
 ),
 
 imputation_five_minsc as (
