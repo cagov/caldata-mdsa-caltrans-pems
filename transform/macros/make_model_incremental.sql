61044fd45d39b285e3ecf161bce5f2b9fbad69b2
Ian Rose <ian.rose@innovation.ca.gov>
2025-03-06 10:57:56 -0800

    Revert

--- transform/macros/make_model_incremental.sql
+++ transform/macros/make_model_incremental.sql
@@ -12,6 +12,18 @@
                     )
                 from {{ this }}
             )
+            {% if target.name != 'prd' %}
+                and {{ date_col }} >= (
+                    dateadd(
+                        day,
+                        {{ var("dev_model_look_back") }},
+                        current_date()
+                    )
+                )
+            {% endif %}
+    {% elif target.name != 'prd' %}
+        {{ date_col }} >= dateadd(day, {{ var("dev_model_look_back") }}, current_date())
+
     {% else %}
     1=1
     {% endif %}
