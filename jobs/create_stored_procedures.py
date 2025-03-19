701925ebf58ed6762dacd9af6a4577a29e5b8088
Ian Rose <ian.rose@innovation.ca.gov>
2025-03-19 12:26:46 -0700

    Create stored procedure to write Iceberg metadata

--- jobs/create_stored_procedures.py
+++ jobs/create_stored_procedures.py
@@ -46,6 +46,47 @@ def unload_as_geojson(
     )
 
 
+def write_iceberg_metadata(session: snowpark.Session, database: str, stage: str):
+    # Your code goes here, inside the "main" handler.
+    iceberg_tables = (
+        session.sql(
+            f"""
+                show iceberg tables in database {database}
+            """
+        )
+        .collect_nowait()
+        .to_df()
+        .to_pandas()
+    )
+
+    def _get_meta(record):
+        identifier = ".".join([database, record["schema_name"], record["name"]])
+        result = (
+            session.sql(
+                f"""
+            select system$get_iceberg_table_information('{identifier}')
+        """
+            )
+            .collect_nowait()
+            .result()
+        )
+        return json.loads(result[0][0])["metadataLocation"]
+
+    iceberg_tables = iceberg_tables.assign(
+        schema=iceberg_tables["schema_name"],
+        metadata=iceberg_tables.apply(_get_meta, axis=1),
+        path=iceberg_tables["base_location"],
+    )[["name", "schema", "path", "metadata"]]
+    fname = "/tmp/current_table_versions.json"
+    iceberg_tables.to_json(fname, orient="records")
+    return session.file.put(
+        fname,
+        f"{stage}/iceberg",
+        auto_compress=False,
+        overwrite=True,
+    )
+
+
 if __name__ == "__main__":
     from jobs.utils.snowflake import snowflake_connection_from_environment
 
@@ -68,6 +109,18 @@ if __name__ == "__main__":
         return_type=types.VariantType(),
         input_types=[types.StringType(), types.StringType(), types.StringType()],
         packages=["snowflake-snowpark-python", "geopandas==0.14.2", "shapely"],
+        replace=True,
+        source_code_display=True,
+        is_permanent=True,
+        stage_location="@PEMS_MARTS_INTERNAL",
+    )
+
+    session.sproc.register(
+        func=write_iceberg_metadata,
+        name="write_iceberg_metadata",
+        return_type=types.VariantType(),
+        input_types=[types.StringType(), types.StringType()],
+        packages=["snowflake-snowpark-python"],
         replace=True,
         source_code_display=True,
         is_permanent=True,
