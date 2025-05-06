3ed9dbef741e9d6b9f2bc6eb6f8745491b67e8d9
Ian Rose <ian.rose@innovation.ca.gov>
2025-05-06 08:50:03 -0700

    Refactor missing rows filling. This does a few things:
    1. Converts the missing_rows model to microbatch
    2. Removes an unnecessary join to add station metadata to model.
        This data already exists on the active_stations model, and can be
        brought along in the spine computation.
    3. Removes an unnecessary join to the g_factor speed model. I'm pretty
       sure we can compute the g factor speed downstream of this model
       without any joins.
    4. Removes unnecesseary coalesce statements: the metadata we are
       coalescing comes from the same place. If there are any discrepancies,
       it would be due to a bad join, which we would want to fix instead of
       coalescing.

--- transform/models/intermediate/clearinghouse/int_clearinghouse__detector_g_factor_based_speed.sql
+++ transform/models/intermediate/clearinghouse/int_clearinghouse__detector_g_factor_based_speed.sql
@@ -19,7 +19,7 @@ detector_agg as (
         updated_volume_sum as volume_sum,
         updated_occupancy_avg as occupancy_avg,
         volume_observed
-    from {{ ref('int_clearinghouse__detector_outlier_agg_five_minutes') }}
+    from {{ ref('int_clearinghouse__detector_agg_five_minutes_normalized') }}
     where {{ make_model_incremental('sample_date') }}
 ),
 
